22fedbf77b33dbc182ff193d09ce34fc2e60e350
Esau Macias <hecttoressau@gmail.com>
2022-07-06 10:43:41 -0500

    feat(jest): configuration for jest behavior

---
+++ packages/api-gateway/src/test.js
@@ -0,0 +1,5 @@
+const addition = (addend1, adden2) => {
+    return addend1 + adden2;
+} 
+
+module.exports = addition;
--- src/packages/api-gateway/__tests__/api-gateway.test.js
+++ src/packages/api-gateway/__tests__/api-gateway.test.js
@@ -1,7 +1,11 @@
-'use strict';
+const addition = require('../src/test');
 
-const apiGateway = require('..');
-
-describe('api-gateway', () => {
-    it('needs tests');
+describe('given a set of numbers', () => {
+    const adden2 = 4;
+    const adden1 = 5
+    describe('when we call addition function', () => {
+        test("then adition should return its sum", () => {
+            expect(addition(adden1, adden2)).toEqual(9)
+        });
+    });
 });
