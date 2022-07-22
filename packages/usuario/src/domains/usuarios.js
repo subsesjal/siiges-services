077002962cb5541a328602549fd3811dbcd7559b
cetherdev <robert.hct@gmail.com>
2022-07-22 12:02:27 -0500

    feat: create method to insert a user in database

--- packages/usuario/src/domains/usuarios.js
+++ packages/usuario/src/domains/usuarios.js
@@ -7,29 +7,95 @@ const findAllQuery = (usuarioModel) => async () => {
         [Op.is]: null,
       },
     },
-    /* include: [
+    include: [
       {
-        association: 'customer',
-        include: ['user'],
+        association: 'persona',
       },
-    ], */
+    ],
   });
 
   return usuarios;
 };
 
 const findOneQuery = (usuarioModel) => async (id) => {
-  const usuario = await usuarioModel.findByPk(id);
+  const usuario = await usuarioModel.findByPk(id, {
+    where: {
+      deletedAt: {
+        [Op.is]: null,
+      },
+    },
+    include: [
+      {
+        association: 'persona',
+      },
+    ],
+  });
+  return usuario;
+};
+
+const findOneDetailedQuery = (usuarioModel) => async (id) => {
+  const usuario = await usuarioModel.findByPk(id, {
+    where: {
+      deletedAt: {
+        [Op.is]: null,
+      },
+    },
+    include: [
+      {
+        association: 'persona',
+        include: [
+          {
+            association: 'domicilio',
+            include: [
+              {
+                association: 'estado',
+              },
+              {
+                association: 'municipio',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  });
   return usuario;
 };
 
 const createQuery = (usuarioModel) => async (data) => {
-  const newUsuario = await usuarioModel.create(data);
+  let newUsuario;
+  if (!data.persona) {
+    const persona = {
+      nombre: 'SIN DATO',
+      apellidoPaterno: 'SIN DATO',
+    };
+    const newData = { ...data, persona };
+
+    newUsuario = await usuarioModel.create(newData, {
+      include: [
+        {
+          association: 'persona',
+          include: ['domicilio'],
+        },
+      ],
+    });
+  } else {
+    newUsuario = await usuarioModel.create(data, {
+      include: [
+        {
+          association: 'persona',
+          include: ['domicilio'],
+        },
+      ],
+    });
+  }
+
   return newUsuario;
 };
 
 module.exports = {
   findAllQuery,
   findOneQuery,
+  findOneDetailedQuery,
   createQuery,
 };
