db27d5590b0e5fa8a0eedcbed19b0267ff5e8ae2
cetherdev <robert.hct@gmail.com>
2022-07-28 12:37:09 -0500

    refactor: add logger to api-gateway

--- packages/api-gateway/src/drivers/http/adapters/usuarios/handlers.js
+++ packages/api-gateway/src/drivers/http/adapters/usuarios/handlers.js
@@ -1,112 +1,112 @@
-const errorHandler = require('../../utils/errorHandler');
 const { Logger } = require('@siiges-services/shared');
+const errorHandler = require('../../utils/errorHandler');
 
 // usuario services
 async function findAllUsuarios(req, reply) {
-	try {
-		Logger.info(`[usuarios]: Getting usuarios list`);
-		const usuarios = await this.usuarioServices.findAllUsuarios();
+  try {
+    Logger.info('[usuarios]: Getting usuarios list');
+    const usuarios = await this.usuarioServices.findAllUsuarios();
 
-		return reply
-			.code(200)
-			.header('Content-Type', 'application/json; charset=utf-8')
-			.send({ data: usuarios });
-	} catch (error) {
-		return errorHandler(error, reply);
-	}
+    return reply
+      .code(200)
+      .header('Content-Type', 'application/json; charset=utf-8')
+      .send({ data: usuarios });
+  } catch (error) {
+    return errorHandler(error, reply);
+  }
 }
 
 async function findOneUsuario(req, reply) {
-	try {
-		const { usuarioId } = req.params;
+  try {
+    const { usuarioId } = req.params;
 
-		Logger.info(`[usuarios]: Getting usuario ${usuarioId}`);
-		const usuario = await this.usuarioServices.findOneUsuario(usuarioId);
+    Logger.info(`[usuarios]: Getting usuario ${usuarioId}`);
+    const usuario = await this.usuarioServices.findOneUsuario(usuarioId);
 
-		return reply
-			.code(200)
-			.header('Content-Type', 'application/json; charset=utf-8')
-			.send({ data: usuario });
-	} catch (error) {
-		return errorHandler(error, reply);
-	}
+    return reply
+      .code(200)
+      .header('Content-Type', 'application/json; charset=utf-8')
+      .send({ data: usuario });
+  } catch (error) {
+    return errorHandler(error, reply);
+  }
 }
 
 async function findOneDetailedUsuario(req, reply) {
-	try {
-		const { usuarioId } = req.params;
+  try {
+    const { usuarioId } = req.params;
 
-		Logger.info(`[usuarios]: Getting usuario: ${usuarioId}`);
-		const usuario = await this.usuarioServices.findOneUsuarioDetailed(
-			usuarioId
-		);
+    Logger.info(`[usuarios]: Getting usuario: ${usuarioId}`);
+    const usuario = await this.usuarioServices.findOneUsuarioDetailed(
+      usuarioId,
+    );
 
-		return reply
-			.code(200)
-			.header('Content-Type', 'application/json; charset=utf-8')
-			.send({ data: usuario });
-	} catch (error) {
-		return errorHandler(error, reply);
-	}
+    return reply
+      .code(200)
+      .header('Content-Type', 'application/json; charset=utf-8')
+      .send({ data: usuario });
+  } catch (error) {
+    return errorHandler(error, reply);
+  }
 }
 
 async function createUsuario(req, reply) {
-	try {
-		const { body } = req;
+  try {
+    const { body } = req;
 
-		Logger.info(`[usuarios]: Creating usuario`);
-		const newUsuario = await this.usuarioServices.createUsuario(body);
+    Logger.info('[usuarios]: Creating usuario');
+    const newUsuario = await this.usuarioServices.createUsuario(body);
 
-		return reply
-			.code(201)
-			.header('Content-Type', 'application/json; charset=utf-8')
-			.send({ data: newUsuario });
-	} catch (error) {
-		return errorHandler(error, reply);
-	}
+    return reply
+      .code(201)
+      .header('Content-Type', 'application/json; charset=utf-8')
+      .send({ data: newUsuario });
+  } catch (error) {
+    return errorHandler(error, reply);
+  }
 }
 
 async function updateUsuario(req, reply) {
-	try {
-		const { usuarioId } = req.params;
-		const { body } = req;
+  try {
+    const { usuarioId } = req.params;
+    const { body } = req;
 
-		Logger.info(`[usuarios]: Updating usuario`);
-		const usuarioUpdated = await this.usuarioServices.updateUsuario(
-			usuarioId,
-			body
-		);
+    Logger.info('[usuarios]: Updating usuario');
+    const usuarioUpdated = await this.usuarioServices.updateUsuario(
+      usuarioId,
+      body,
+    );
 
-		return reply
-			.code(201)
-			.header('Content-Type', 'application/json; charset=utf-8')
-			.send({ data: usuarioUpdated });
-	} catch (error) {
-		return errorHandler(error, reply);
-	}
+    return reply
+      .code(201)
+      .header('Content-Type', 'application/json; charset=utf-8')
+      .send({ data: usuarioUpdated });
+  } catch (error) {
+    return errorHandler(error, reply);
+  }
 }
 
 async function deleteUsuario(req, reply) {
-	try {
-		const { usuarioId } = req.params;
+  try {
+    const { usuarioId } = req.params;
 
-		Logger.info(`[usuarios]: Deleting usuario: ${usuarioId}`);
-		const usuarioDeleted = await this.usuarioServices.deleteUsuario(usuarioId);
+    Logger.info(`[usuarios]: Deleting usuario: ${usuarioId}`);
+    const usuarioDeleted = await this.usuarioServices.deleteUsuario(usuarioId);
 
-		return reply
-			.code(201)
-			.header('Content-Type', 'application/json; charset=utf-8')
-			.send({ data: usuarioDeleted });
-	} catch (error) {
-		return errorHandler(error, reply);
-	}
+    return reply
+      .code(201)
+      .header('Content-Type', 'application/json; charset=utf-8')
+      .send({ data: usuarioDeleted });
+  } catch (error) {
+    return errorHandler(error, reply);
+  }
 }
 
 module.exports = {
-	findAllUsuarios,
-	findOneUsuario,
-	findOneDetailedUsuario,
-	createUsuario,
-	updateUsuario,
-	deleteUsuario,
+  findAllUsuarios,
+  findOneUsuario,
+  findOneDetailedUsuario,
+  createUsuario,
+  updateUsuario,
+  deleteUsuario,
 };
--- packages/api-gateway/src/drivers/http/server.js
+++ packages/api-gateway/src/drivers/http/server.js
@@ -4,12 +4,12 @@ const helmet = require('@fastify/helmet');
 const Fastify = require('fastify');
 const cors = require('@fastify/cors');
 const path = require('path');
-const {
-	serverHost,
-	whiteList,
-	serverPort,
-} = require('../../../config/environment');
 const { Logger } = require('@siiges-services/shared');
+const {
+  serverHost,
+  whiteList,
+  serverPort,
+} = require('../../../config/environment');
 
 // Internal dependencies
 // const authDecorators = require('./decorators/auth');
@@ -18,8 +18,8 @@ const { Logger } = require('@siiges-services/shared');
 const isTestEnv = process.env.NODE_ENV === 'test';
 
 const fastify = Fastify({
-	// Disable logs in test enviroment
-	logger: !isTestEnv,
+  // Disable logs in test enviroment
+  logger: !isTestEnv,
 });
 
 // Avoid loading swagger when running tests
@@ -29,17 +29,17 @@ const fastify = Fastify({
 } */
 
 const options = {
-	origin: (origin, cb) => {
-		if (whiteList.includes(origin)) {
-			cb(null, true);
-		} else {
-			cb(new Error('Not allowed'), false);
-		}
-	},
+  origin: (origin, cb) => {
+    if (whiteList.includes(origin)) {
+      cb(null, true);
+    } else {
+      cb(new Error('Not allowed'), false);
+    }
+  },
 };
 
 fastify.register(cors, {
-	options,
+  options,
 });
 
 // Decorators for authorization
@@ -49,30 +49,30 @@ fastify.decorate('hasRole', authDecorators.hasRole);
 fastify.register(helmet);
 
 fastify.register(autoLoad, {
-	dir: path.join(__dirname, 'routes'),
-	ignorePattern: /.*(schema).*/,
-	options: { prefix: 'api/v1' },
+  dir: path.join(__dirname, 'routes'),
+  ignorePattern: /.*(schema).*/,
+  options: { prefix: 'api/v1' },
 });
 
 fastify.register(autoLoad, { dir: path.join(__dirname, 'plugin') });
 
 async function start() {
-	await fastify.listen(
-		{
-			port: serverPort,
-			host: serverHost,
-		},
-		(err, address) => {
-			if (err) {
-				Logger.error(`[http-server]: Error with ${err.message} has happend`);
-				process.exit(1);
-			}
-			Logger.info(`Server listening at ${address}`);
-		}
-	);
+  await fastify.listen(
+    {
+      port: serverPort,
+      host: serverHost,
+    },
+    (err, address) => {
+      if (err) {
+        Logger.error(`[http-server]: Error with ${err.message} has happend`);
+        process.exit(1);
+      }
+      Logger.info(`Server listening at ${address}`);
+    },
+  );
 }
 
 module.exports = {
-	start,
-	fastify,
+  start,
+  fastify,
 };
