d8615258e6d35cee1920c411a482335e13d51361
cetherdev <robert.hct@gmail.com>
2022-08-02 23:49:16 -0500

    feat: add swagger config and documentation of usuario endpoints

--- packages/api-gateway/src/drivers/http/server.js
+++ packages/api-gateway/src/drivers/http/server.js
@@ -6,60 +6,60 @@ const cors = require('@fastify/cors');
 const path = require('path');
 const { Logger } = require('@siiges-services/shared');
 const {
-	serverHost,
-	whiteList,
-	serverPort,
+  serverHost,
+  whiteList,
+  serverPort,
 } = require('../../../config/environment');
 
 const fastify = Fastify({
-	ajv: {
-		customOptions: {
-			allErrors: true,
-		},
-	},
-	logger: true,
+  ajv: {
+    customOptions: {
+      allErrors: true,
+    },
+  },
+  logger: true,
 });
 
 fastify.register(helmet);
 
 fastify.register(AutoLoad, {
-	dir: path.join(__dirname, 'plugins'),
+  dir: path.join(__dirname, 'plugins'),
 });
 
 const options = {
-	origin: (origin, cb) => {
-		if (whiteList.includes(origin)) {
-			cb(null, true);
-		} else {
-			cb(new Error('Not allowed'), false);
-		}
-	},
+  origin: (origin, cb) => {
+    if (whiteList.includes(origin)) {
+      cb(null, true);
+    } else {
+      cb(new Error('Not allowed'), false);
+    }
+  },
 };
 
 fastify.register(cors, {
-	options,
+  options,
 });
 
 fastify.register(AutoLoad, {
-	dir: path.join(__dirname, 'routes'),
-	ignorePattern: /.*(schema).*/,
-	options: { prefix: 'api/v1' },
+  dir: path.join(__dirname, 'routes'),
+  ignorePattern: /.*(schema).*/,
+  options: { prefix: 'api/v1' },
 });
 
 const start = async () => {
-	await fastify.listen(
-		{
-			port: serverPort,
-			host: serverHost,
-		},
-		(err, address) => {
-			if (err) {
-				Logger.error(`[http-server]: Error with ${err.message} has happend`);
-				process.exit(1);
-			}
-			Logger.info(`Server listening at ${address}`);
-		}
-	);
+  await fastify.listen(
+    {
+      port: serverPort,
+      host: serverHost,
+    },
+    (err, address) => {
+      if (err) {
+        Logger.error(`[http-server]: Error with ${err.message} has happend`);
+        process.exit(1);
+      }
+      Logger.info(`Server listening at ${address}`);
+    },
+  );
 };
 
 module.exports = { start, fastify };
