00f422b9413316fb5e8c87f5fac6d0055d985f63
cetherdev <robert.hct@gmail.com>
2022-08-04 11:50:40 -0500

    fix(usuario/domains): change the findByPk by findOne method

--- packages/usuario/src/domains/usuarios.js
+++ packages/usuario/src/domains/usuarios.js
@@ -2,170 +2,191 @@ const { Op } = require('sequelize');
 const boom = require('@hapi/boom');
 
 const findAllQuery = (usuarioModel) => async () => {
-  const usuarios = await usuarioModel.findAll({
-    where: {
-      deletedAt: {
-        [Op.is]: null,
-      },
-    },
-    include: [
-      {
-        association: 'persona',
-      },
-    ],
-  });
+	const usuarios = await usuarioModel.findAll({
+		where: {
+			deletedAt: {
+				[Op.is]: null,
+			},
+		},
+		include: [
+			{
+				association: 'persona',
+			},
+		],
+	});
 
-  return usuarios;
+	return usuarios;
 };
 
 const findOneQuery = (usuarioModel) => async (id) => {
-  const usuario = await usuarioModel.findByPk(id, {
-    where: {
-      deletedAt: {
-        [Op.is]: null,
-      },
-    },
-    include: [
-      {
-        association: 'persona',
-      },
-    ],
-  });
-  return usuario;
+	const usuario = await usuarioModel.findOne({
+		where: {
+			id,
+			deletedAt: {
+				[Op.is]: null,
+			},
+		},
+		include: [
+			{
+				association: 'persona',
+			},
+		],
+	});
+
+	console.log(usuario);
+
+	return usuario;
 };
 
 const findOneDetailedQuery = (usuarioModel) => async (id) => {
-  const usuario = await usuarioModel.findByPk(id, {
-    where: {
-      deletedAt: {
-        [Op.is]: null,
-      },
-    },
-    include: [
-      {
-        association: 'persona',
-        include: [
-          {
-            association: 'domicilio',
-            include: [
-              {
-                association: 'estado',
-              },
-              {
-                association: 'municipio',
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  });
-  return usuario;
+	const usuario = await usuarioModel.findOne({
+		where: {
+			id,
+			deletedAt: {
+				[Op.is]: null,
+			},
+		},
+		include: [
+			{
+				association: 'persona',
+				include: [
+					{
+						association: 'domicilio',
+						include: [
+							{
+								association: 'estado',
+							},
+							{
+								association: 'municipio',
+							},
+						],
+					},
+				],
+			},
+		],
+	});
+	return usuario;
 };
 
 const createQuery = (usuarioModel) => async (data) => {
-  let newUsuario;
-  if (!data.persona) {
-    const persona = {
-      nombre: 'SIN DATO',
-      apellidoPaterno: 'SIN DATO',
-    };
-    const newData = { ...data, persona };
+	let newUsuario;
+	if (!data.persona) {
+		const persona = {
+			nombre: 'SIN DATO',
+			apellidoPaterno: 'SIN DATO',
+		};
+		const newData = { ...data, persona };
 
-    newUsuario = await usuarioModel.create(newData, {
-      include: [
-        {
-          association: 'persona',
-          include: ['domicilio'],
-        },
-      ],
-    });
-  } else {
-    newUsuario = await usuarioModel.create(data, {
-      include: [
-        {
-          association: 'persona',
-          include: ['domicilio'],
-        },
-      ],
-    });
-  }
+		newUsuario = await usuarioModel.create(newData, {
+			include: [
+				{
+					association: 'persona',
+					include: ['domicilio'],
+				},
+			],
+		});
+	} else {
+		newUsuario = await usuarioModel.create(data, {
+			include: [
+				{
+					association: 'persona',
+					include: ['domicilio'],
+				},
+			],
+		});
+	}
 
-  return newUsuario;
+	return newUsuario;
 };
 
 const updateQuery = (usuarioModel, personaModel) => async (id, changes) => {
-  const usuario = await usuarioModel.findByPk(id, {
-    where: {
-      deletedAt: {
-        [Op.is]: null,
-      },
-    },
-  });
+	const usuario = await usuarioModel.findOne({
+		where: {
+			id,
+			deletedAt: {
+				[Op.is]: null,
+			},
+		},
+	});
 
-  if (!usuario) {
-    throw boom.notFound(
-      `[usuarios:finOne]: Usuario no encontrado con id: ${id}`,
-    );
-  }
+	if (!usuario) {
+		throw boom.notFound(
+			`[usuarios:finOne]: Usuario no encontrado con id: ${id}`
+		);
+	}
 
-  const persona = await personaModel.findByPk(usuario.personaId, {
-    where: {
-      deletedAt: {
-        [Op.is]: null,
-      },
-    },
-  });
+	const persona = await personaModel.findOne({
+		where: {
+			id: usuario.personaId,
+			deletedAt: {
+				[Op.is]: null,
+			},
+		},
+	});
 
-  const updatedAt = new Date().toISOString();
+	if (!persona) {
+		throw boom.notFound(
+			`[usuarios:finOne]: Usuario-persona no encontrado con id: ${usuario.personaId}`
+		);
+	}
 
-  const usuarioChanges = { ...changes, updatedAt };
-  const personaChanges = { ...changes.persona, updatedAt };
+	const updatedAt = new Date().toISOString();
 
-  const usuarioUpdated = await usuario.update(usuarioChanges);
-  const personaUpdated = await persona.update(personaChanges);
-  usuarioUpdated.dataValues.persona = personaUpdated;
+	const usuarioChanges = { ...changes, updatedAt };
+	const personaChanges = { ...changes.persona, updatedAt };
 
-  return usuarioUpdated;
+	const usuarioUpdated = await usuario.update(usuarioChanges);
+	const personaUpdated = await persona.update(personaChanges);
+	usuarioUpdated.dataValues.persona = personaUpdated;
+
+	return usuarioUpdated;
 };
 
 const deleteQuery = (usuarioModel, personaModel) => async (id) => {
-  const usuario = await usuarioModel.findByPk(id, {
-    where: {
-      deletedAt: {
-        [Op.is]: null,
-      },
-    },
-  });
+	const usuario = await usuarioModel.findOne({
+		where: {
+			id,
+			deletedAt: {
+				[Op.is]: null,
+			},
+		},
+	});
 
-  if (!usuario) {
-    throw boom.notFound(
-      `[usuarios:finOne]: Usuario no encontrado con id: ${id}`,
-    );
-  }
+	if (!usuario) {
+		throw boom.notFound(
+			`[usuarios:finOne]: Usuario no encontrado con id: ${id}`
+		);
+	}
 
-  const persona = await personaModel.findByPk(usuario.personaId, {
-    where: {
-      deletedAt: {
-        [Op.is]: null,
-      },
-    },
-  });
+	const persona = await personaModel.findOne({
+		where: {
+			id: usuario.personaId,
+			deletedAt: {
+				[Op.is]: null,
+			},
+		},
+	});
 
-  const deletedAt = new Date().toISOString();
+	if (!persona) {
+		throw boom.notFound(
+			`[usuarios:finOne]: Usuario-persona no encontrado con id: ${usuario.personaId}`
+		);
+	}
 
-  const usuarioDeleted = await usuario.update({ deletedAt });
-  const personaDeleted = await persona.update({ deletedAt });
-  usuarioDeleted.dataValues.persona = personaDeleted;
+	const deletedAt = new Date().toISOString();
 
-  return usuarioDeleted;
+	const usuarioDeleted = await usuario.update({ deletedAt });
+	const personaDeleted = await persona.update({ deletedAt });
+	usuarioDeleted.dataValues.persona = personaDeleted;
+
+	return usuarioDeleted;
 };
 
 module.exports = {
-  findAllQuery,
-  findOneQuery,
-  findOneDetailedQuery,
-  createQuery,
-  updateQuery,
-  deleteQuery,
+	findAllQuery,
+	findOneQuery,
+	findOneDetailedQuery,
+	createQuery,
+	updateQuery,
+	deleteQuery,
 };
