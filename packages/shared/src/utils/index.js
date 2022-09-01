a0aaa66acefb9315ff0ae3f76072abd19f1694c7
Hector Esau <hecttoressau@gmail.com>
2022-08-31 19:38:47 -0500

    refactor(shared/logger): change logger behaviour to store logs in files

--- packages/shared/src/utils/index.js
+++
@@ -1,9 +0,0 @@
-const constants = require('./constants');
-const Logger = require('./logger');
-const checkers = require('./checkers');
-
-module.exports = {
-  checkers,
-  constants,
-  Logger,
-};
--- packages/shared/src/utils/logger/index.js
+++ packages/shared/src/utils/logger/index.js
@@ -1,10 +1,29 @@
+// External depenendecies
 const Winston = require('winston');
+const { join } = require('path');
+const { getEnvironment } = require('../../adapters/nodejs');
+const { isProdEnvironment } = require('../checkers');
 
 const Logger = Winston.createLogger({
-  format: Winston.format.json(),
-  transport: [
-    new Winston.transports.Console(),
+  format: Winston.format.combine(
+    Winston.format.timestamp(),
+    Winston.format.json(),
+  ),
+  transports: [
+    new Winston.transports.File({
+      filename: join('../../../../../', 'error.log'),
+      level: 'error',
+    }),
+    new Winston.transports.File({
+      filename: join('../../../../../', 'combined.log'),
+    }),
   ],
 });
+
+if (!isProdEnvironment(getEnvironment())) {
+  Logger.add(new Winston.transports.Console({
+    format: Winston.format.simple(),
+  }));
+}
 
 module.exports = Logger;
