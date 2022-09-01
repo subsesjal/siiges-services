5bc00424f7884d7c50e6784ec1bff6bd6855eae7
Hector Esau <hecttoressau@gmail.com>
2022-09-01 10:43:37 -0500

    feat(core): rename env vars to keep the standard

--- packages/core/config/config.js
+++ packages/core/config/config.js
@@ -2,11 +2,11 @@ const { dotenv } = require('@siiges-services/shared');
 
 const mysql = {
   dbAdmin: 'mysql',
-  dbHost: dotenv.getEnvironmentVar('DB_HOST'),
+  dbHost: dotenv.getEnvironmentVar('DB_HOST_MYSQL'),
   dbPort: dotenv.getEnvironmentVar('DB_PORT_MYSQL'),
   dbUser: dotenv.getEnvironmentVar('DB_USER_MYSQL'),
   dbPassword: dotenv.getEnvironmentVar('DB_PASSWORD_MYSQL'),
-  dbName: dotenv.getEnvironmentVar('DB_NAME'),
+  dbName: dotenv.getEnvironmentVar('DB_NAME_MYSQL'),
 };
 
 module.exports = {
