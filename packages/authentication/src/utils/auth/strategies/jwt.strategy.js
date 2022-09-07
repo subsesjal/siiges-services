0fbe5c5ca24c9c45d008a97b7a8c7dafcbd88593
Hector Esau <49880344+hecttoressau@users.noreply.github.com>
2022-09-07 13:02:23 -0500

    Feat/auth utils (#51)
    * refactor(shared/logger): change logger behaviour to store logs in files
    
    * feat(shared/validate): ccreate validate function to validate the var datatype
    
    * refactor(auth): remove files in utils to put them in a better folder
    
    * feat(auth/constants): created constants needed to auth package work
    
    * feat(auth/utils): create function to validate user token payload

--- packages/authentication/src/utils/auth/strategies/jwt.strategy.js
+++
@@ -1,9 +0,0 @@
-// External dependencies
-const { Strategy } = require('passport-jwt');
-
-// Internal dependencies
-const { jwtSecret } = require('../../../../config');
-
-const JwtStrategy = new Strategy(jwtSecret, (payload, done) => done(null, payload));
-
-module.exports = JwtStrategy;
