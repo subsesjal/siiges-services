b512a7aa5f89cdfe56dff71a8517222b466e74ed
Hector Esau <hecttoressau@gmail.com>
2022-09-12 12:36:48 -0500

    feat(core/adapters.create): function to update one  entry from table in db

---
+++ packages/core/src/adapters/db/update.db.adapters.js
@@ -0,0 +1,25 @@
+// External dependencies
+const boom = require('@hapi/boom');
+// Internal dependencies
+const findOneQuery = require('./find-one.db.adapter');
+
+const updateQuery = (model) => async (id, changes) => {
+  const tableName = model.getTableName();
+  const findOne = findOneQuery(model);
+  const actualEntry = await findOne({ id });
+
+  if (!actualEntry) {
+    throw boom.notFound(
+      `[${tableName}:finOne]: We couldn't find any entry with identifier ${id} in \
+${tableName} table`,
+    );
+  }
+
+  const updatedAt = new Date().toISOString();
+  const entryChanges = { ...changes, updatedAt };
+  const entryUpdated = await model.update(entryChanges);
+
+  return entryUpdated;
+};
+
+module.exports = updateQuery;
