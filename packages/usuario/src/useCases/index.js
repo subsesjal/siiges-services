3236c81224223a317768765b6a04dff550d67d23
Hector Esau <hecttoressau@gmail.com>
2022-09-12 16:56:05 -0500

    refactor(usuarios/export): refactor usuarios package to adapt to new utilies

--- packages/usuario/src/useCases/index.js
+++ packages/usuario/src/useCases/index.js
@@ -1,17 +1,21 @@
 const {
-  findOneUsuario,
-  findOneUsuarioDetailed,
-  findAllUsuarios,
-  createUsuario,
-  updateUsuario,
-  deleteUsuario,
-} = require('./usuarios');
+  createQuery,
+  findAllQuery,
+  findOneQuery,
+  deleteQuery,
+  updateQuery,
+} = require('../adapters/db');
+
+const findOneUser = require('./db/find-one.users.use-cases');
+const findAllUsers = require('./db/find-all.users.use-cases');
+const createUser = require('./db/create.users.use-cases');
+const deleteUser = require('./db/delete.users.use-cases');
+const updateUser = require('./db/update.users.use-cases');
 
 module.exports = {
-  findOneUsuario,
-  findOneUsuarioDetailed,
-  findAllUsuarios,
-  createUsuario,
-  updateUsuario,
-  deleteUsuario,
+  findAllUsers: findAllUsers(findAllQuery),
+  findOneUser: findOneUser(findOneQuery),
+  createUser: createUser(createQuery),
+  updateUser: updateUser(updateQuery),
+  deleteUser: deleteUser(deleteQuery),
 };
