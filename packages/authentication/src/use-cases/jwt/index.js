84eec3f71773160860e55abd44b1a12f69b379f6
Hector Esau <hecttoressau@gmail.com>
2022-09-19 12:08:40 -0500

    refactor(authentication): modify to align better to clean architecture

--- packages/authentication/src/use-cases/jwt/index.js
+++
@@ -1,7 +0,0 @@
-const signTokenModule = require('./sign-token');
-const createPayloadsModule = require('./create-token-payloads');
-
-module.exports = {
-  ...signTokenModule,
-  ...createPayloadsModule,
-};
