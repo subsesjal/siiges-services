110ec4e0bb357d669fa48b0646d80525cd06f7b0
Hector Esau <hecttoressau@gmail.com>
2022-09-23 11:20:58 -0500

    refactor(core/adapters): deleted repeated code and modify update and delete

--- packages/core/src/adapters/db/delete.db.adapters.js
+++ packages/core/src/adapters/db/delete.db.adapters.js
@@ -1,28 +1,12 @@
 // Internal dependencies
-const { updateQuery, findAndUpdateQuery } = require('./update.db.adapters');
+const { updateQuery } = require('./update.db.adapters');
 
 const deleteQuery = (model) => async (identifierObj) => {
   const update = updateQuery(model);
   const deletedAt = new Date().toISOString();
-
-  const entryDeleted = await update(identifierObj, { deletedAt });
-<<<<<<< HEAD
-=======
-
->>>>>>> 54af6d9 (feat(core/adapters):  refactor core adapters to be more general)
-  return entryDeleted;
-};
-
-const findAndDeleteQuery = (model) => async (identifierObj) => {
-  const update = findAndUpdateQuery(model);
-  const deletedAt = new Date().toISOString();
-
   const entryDeleted = await update(identifierObj, { deletedAt });
 
   return entryDeleted;
 };
 
-module.exports = {
-  deleteQuery,
-  findAndDeleteQuery,
-};
+module.exports = deleteQuery;
--- packages/core/src/adapters/db/index.js
+++ packages/core/src/adapters/db/index.js
@@ -1,5 +1,5 @@
 const createQuery = require('./create.adapters');
-const { deleteQuery } = require('./delete.db.adapters');
+const deleteQuery = require('./delete.db.adapters');
 const findOneQuery = require('./find-one.db.adapter');
 const findAllQuery = require('./find-all.db.adapter');
 const { updateQuery } = require('./update.db.adapters');
