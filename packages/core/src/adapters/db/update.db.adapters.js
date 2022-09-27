9dee109e777c0dd8ffb56a88a9c4458c28ac005f
Hector Esau <hecttoressau@gmail.com>
2022-09-27 13:55:14 -0500

    fix(core/update): fix update logic problems

--- packages/core/src/adapters/db/update.db.adapters.js
+++ packages/core/src/adapters/db/update.db.adapters.js
@@ -1,7 +1,18 @@
-const updateQuery = (model) => async (identifierObj, changes) => {
+const { Op } = require('sequelize');
+
+const updateQuery = (model) => async (identifierObj, changes, includes) => {
   const updatedAt = new Date().toISOString();
   const entryChanges = { ...changes, updatedAt };
-  const entryUpdated = await model.update(entryChanges, { ...identifierObj });
+  const entryUpdated = await model.update(
+    entryChanges,
+    {
+      where: {
+        ...identifierObj,
+        deletedAt: { [Op.is]: null },
+      },
+    },
+    includes,
+  );
 
   return entryUpdated;
 };
