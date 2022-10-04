b128d7252e17394c118d6ae9cf06bebf3c51f32c
Hector Esau <hecttoressau@gmail.com>
2022-10-04 09:36:55 -0500

    refactor(create-include): now we can make inner or left joins in our searches

--- packages/core/src/adapters/utils/create-indclude.utils.js
+++ packages/core/src/adapters/utils/create-indclude.utils.js
@@ -1,31 +1,33 @@
 const { Op } = require('sequelize');
 const { checkers } = require('@siiges-services/shared');
 
-function createIncludeObj(include) {
-  return {
-    associatation: include,
-  };
-}
-
-function addDeleteAt(include) {
+function addDeleteAt(include, strict) {
   return {
     ...include,
-    deletedAt: { [Op.is]: null },
+    where: { deletedAt: { [Op.is]: null } },
+    required: strict,
   };
 }
 
 function getIncludeObj(include) {
-  if (checkers.isString(include)) return createIncludeObj(include);
+  if (checkers.isString(include)) return { association: include };
   return { ...include };
 }
 
-function createInclude(includeList) {
-  if (checkers.isFalsy(includeList)) return includeList;
+function createInclude(includeList, strict) {
+  if (!Array.isArray(includeList)) return undefined;
 
   return includeList.map((include) => {
-    if (checkers.isDefined(include?.include)) return createInclude(include.include);
-    const localInclude = getIncludeObj(include);
-    return addDeleteAt(localInclude);
+    let localInclude = getIncludeObj(include);
+    localInclude = addDeleteAt(localInclude, strict);
+    if (checkers.isDefined(include?.include)) {
+      return {
+        ...localInclude,
+        include: createInclude(include.include, strict),
+      };
+    }
+
+    return { ...localInclude };
   });
 }
 
