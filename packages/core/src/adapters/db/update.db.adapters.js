e91fb19e9e0ee89e7e018d78721d998290ae0a5d
Hector Esau <hecttoressau@gmail.com>
2022-10-18 11:45:11 -0500

    feat(core): add a new adapter to make update return the entry updated

--- packages/core/src/adapters/db/update.db.adapters.js
+++ packages/core/src/adapters/db/update.db.adapters.js
@@ -1,9 +1,13 @@
+// External dependencies
 const { Op } = require('sequelize');
+// Internal dependencies
+const findOneQuery = require('./find-one.db.adapter');
 
 const updateQuery = (model) => async (identifierObj, changes) => {
   const updatedAt = new Date().toISOString();
   const entryChanges = { ...changes, updatedAt };
-  const entryUpdated = await model.update(
+
+  await model.update(
     entryChanges,
     {
       where: {
@@ -12,8 +16,19 @@ const updateQuery = (model) => async (identifierObj, changes) => {
       },
     },
   );
+};
+
+const updateAndFindQuery = (model) => async (identifierObj, changes) => {
+  const update = updateQuery(model);
+  const findOne = findOneQuery(model);
+
+  await update(identifierObj, changes);
+  const entryUpdated = await findOne(identifierObj);
 
   return entryUpdated;
 };
 
-module.exports = updateQuery;
+module.exports = {
+  updateQuery,
+  updateAndFindQuery,
+};
