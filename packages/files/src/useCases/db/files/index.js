9a851352dd0f15699cb0226eb69b1bf71dfc06e7
Hector Esau <hecttoressau@gmail.com>
2022-10-18 11:54:32 -0500

    feat(files/logger): add more Loggers to inform more about what is happening

--- packages/files/src/useCases/db/files/index.js
+++ packages/files/src/useCases/db/files/index.js
@@ -5,7 +5,7 @@ const {
   updateFileQuery,
 } = require('../../../adapters/db/files.db.adapters');
 
-const { getFileIdentifierObj } = require('./get-identifier-obj.files.db.use-cases');
+const getFileIdentifierObj = require('./get-identifier-obj.files.db.use-cases');
 const createFile = require('./create.files.db.use-cases');
 const deleteFile = require('./delete.files.db.use-cases');
 const findOneFile = require('./find-one.files.db.use-cases');
