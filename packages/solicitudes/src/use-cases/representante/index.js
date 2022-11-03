5f8a50b7a555d059c0641f14675d94e33ed74042
Hector Esau <hecttoressau@gmail.com>
2022-11-03 11:42:37 -0600

    feat(representante): delete files not related to this pr

--- packages/solicitudes/src/use-cases/representante/index.js
+++
@@ -1,8 +0,0 @@
-const { representante } = require('../../adapters');
-const findOne = require('./find-one.representante.use-cases');
-const update = require('./update.representante.use-cases');
-
-module.exports = {
-  findOne: findOne(representante.findOne),
-  update: update(representante.update),
-};
