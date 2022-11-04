5c3bdf8b7438f5bd7d86a21366d57e30b7dc21ef
Hector Esau <hecttoressau@gmail.com>
2022-11-04 16:02:28 -0600

    fix(import/solicitudes): fix a incorrect import in use cases

--- packages/solicitudes/src/use-cases/index.js
+++ packages/solicitudes/src/use-cases/index.js
@@ -1,9 +1,7 @@
 const diligence = require('./diligencia');
 const institution = require('./institucion');
-const representative = require('./representante');
 
 module.exports = {
   diligence,
   institution,
-  representative,
 };
