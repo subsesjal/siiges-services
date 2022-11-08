97912f54f5d89af4d51d34d8c64f69a66d9e579f
Hector Esau <hecttoressau@gmail.com>
2022-11-08 16:30:34 -0600

    feat(usuario/update): created use case to update usuario and persona at the same time

--- packages/usuario/src/useCases/index.js
+++ packages/usuario/src/useCases/index.js
@@ -1,29 +1,9 @@
-const {
-  createQuery,
-  createUserUsersQuery,
-  findAllQuery,
-  findAllUserUsersQuery,
-  findOneQuery,
-  deleteQuery,
-  updateQuery,
-} = require('../adapters/db');
-
-const findOneUser = require('./db/find-one.users.use-cases');
-const findAllUsers = require('./db/find-all.users.use-cases');
-const findAllUserUsers = require('./db/find-all.user-users.use-cases');
-const createUser = require('./db/create.users.use-cases');
-const createUserUser = require('./db/create.user-user.use-casesy');
-const deleteUser = require('./db/delete.users.use-cases');
-const updateUser = require('./db/update.users.use-cases');
-const findOneUserDetail = require('./db/find-one.users-detail.use-cases');
+const updateUserInfo = require('./update-user-info.use-cases');
+const person = require('./persona');
+const usuario = require('./usuario');
 
 module.exports = {
-  findAllUsers: findAllUsers(findAllQuery),
-  findAllUserUsers: findAllUserUsers(findAllUserUsersQuery, findOneQuery),
-  findOneUser: findOneUser(findOneQuery),
-  findOneUserDetail: findOneUserDetail(findOneQuery),
-  createUser: createUser(createQuery),
-  createUserUser: createUserUser(createUserUsersQuery, createQuery, findOneQuery),
-  updateUser: updateUser(updateQuery),
-  deleteUser: deleteUser(deleteQuery),
+  person,
+  updateUserInfo,
+  ...usuario,
 };
