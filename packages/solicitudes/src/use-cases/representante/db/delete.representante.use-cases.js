88db2d5eab3c8bb75ffd12a4acf0b48dc4b3b0c8
Hector Esau <hecttoressau@gmail.com>
2022-11-10 13:53:48 -0600

    chore(delete/representante): change the way we find a deleted entry

--- packages/solicitudes/src/use-cases/representante/db/delete.representante.use-cases.js
+++ packages/solicitudes/src/use-cases/representante/db/delete.representante.use-cases.js
@@ -1,8 +1,9 @@
 const { Logger } = require('@siiges-services/shared');
+const { includeFindOne } = require('../constants');
 
 const deleteOne = (deleteQuery) => async (identifierObj) => {
   Logger.info('[representative/delete] Deleting  representative');
-  const representativeDeleted = await deleteQuery(identifierObj);
+  const representativeDeleted = await deleteQuery(identifierObj, { include: includeFindOne });
   Logger.info('[representative/delete] representative deleted');
 
   return representativeDeleted;
