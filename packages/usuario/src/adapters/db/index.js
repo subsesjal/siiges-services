3af9ff05a342adb565b742d9d88451a460ce23f9
Hector Esau <hecttoressau@gmail.com>
2022-11-10 14:46:16 -0600

    chore(adapters): modify usuario adapters and export persona adapters

--- packages/usuario/src/adapters/db/index.js
+++ packages/usuario/src/adapters/db/index.js
@@ -1,5 +1,6 @@
 // External dependencies
 const { drivers, queries } = require('@siiges-services/core');
+const persona = require('./persona.db.adapters');
 
 const { Usuario, UsuarioUsuario } = drivers.sequelize.models;
 
@@ -8,7 +9,7 @@ const {
   findAllQuery,
   createQuery,
   deleteQuery,
-  updateQuery,
+  updateAndFindQuery,
 } = queries;
 
 module.exports = {
@@ -17,6 +18,7 @@ module.exports = {
   findAllUserUsersQuery: findAllQuery(UsuarioUsuario),
   createQuery: createQuery(Usuario),
   deleteQuery: deleteQuery(Usuario),
-  updateQuery: updateQuery(Usuario),
+  updateQuery: updateAndFindQuery(Usuario),
   createUserUsersQuery: createQuery(UsuarioUsuario),
+  persona,
 };
