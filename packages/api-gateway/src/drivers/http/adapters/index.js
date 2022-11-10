ae608823e3506e6c805dac58781f3788d38fad17
Hector Esau <hecttoressau@gmail.com>
2022-11-10 13:59:21 -0600

    chore(import): remove unnecesaries imports

--- packages/api-gateway/src/drivers/http/adapters/index.js
+++ packages/api-gateway/src/drivers/http/adapters/index.js
@@ -3,10 +3,9 @@ const usuariosAdapter = require('./usuarios/handlers');
 const filesAdapter = require('./files/handlers');
 const institucionesAdapter = require('./institituciones/handlers');
 const solicitudesAdapter = require('./solicitudes/solicitudes.handlers');
-const { diligenceAdapter, representativeAdapter } = require('./solicitud');
+const { representativeAdapter } = require('./solicitud');
 
 module.exports = {
-  diligenceAdapter,
   filesAdapter,
   institucionesAdapter,
   solicitudesAdapter,
--- packages/api-gateway/src/drivers/http/adapters/solicitud/index.js
+++ packages/api-gateway/src/drivers/http/adapters/solicitud/index.js
@@ -1,7 +1,5 @@
-const diligenceAdapter = require('./diligencia/handlers');
 const representativeAdapter = require('./representantes/handler');
 
 module.exports = {
-  diligenceAdapter,
   representativeAdapter,
 };
