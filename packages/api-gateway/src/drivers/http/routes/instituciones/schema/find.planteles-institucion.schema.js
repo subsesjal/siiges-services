e478578b934a73f344167a2083079b95031830ba
cetherdev <robert.hct@gmail.com>
2023-01-30 10:25:27 -0600

    feat: add ratificacion and rector on schema

--- packages/api-gateway/src/drivers/http/routes/instituciones/schema/find.planteles-institucion.schema.js
+++ packages/api-gateway/src/drivers/http/routes/instituciones/schema/find.planteles-institucion.schema.js
@@ -3,7 +3,10 @@ const { plantel } = require('./properties/plantel');
 const { domicilio } = require('./properties/domicilio');
 const { municipio } = require('./properties/municipio');
 const { estado } = require('./properties/estado');
+const { ratificacionNombre } = require('./properties/ratificacionNombre');
 const { responseProperties } = require('./properties/responseProperties');
+const { rector } = require('./properties/rector');
+const { persona } = require('../../usuarios/schema/properties/persona');
 
 const findPlantelesInstitucionSchema = {
   tags: ['Plantel'],
@@ -27,6 +30,30 @@ const findPlantelesInstitucionSchema = {
             usuarioId: { type: 'integer' },
             ...institucion,
             ...responseProperties,
+            rector: {
+              type: ['object', 'null'],
+              properties: {
+                id: { type: 'integer' },
+                ...rector,
+                ...responseProperties,
+                persona: {
+                  type: 'object',
+                  properties: {
+                    id: { type: 'integer' },
+                    ...persona,
+                    ...responseProperties,
+                  },
+                },
+              },
+            },
+            ratificacionesNombre: {
+              type: 'array',
+              properties: {
+                id: { type: 'integer' },
+                ...ratificacionNombre,
+                ...responseProperties,
+              },
+            },
             planteles: {
               type: 'array',
               properties: {
