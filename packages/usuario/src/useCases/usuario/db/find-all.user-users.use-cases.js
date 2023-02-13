e8ebbf9e4bbff0b1897f6ed22cde6d92b8af7255
cetherdev <robert.hct@gmail.com>
2023-02-12 22:32:45 -0600

    fix: get users

--- packages/usuario/src/useCases/usuario/db/find-all.user-users.use-cases.js
+++ packages/usuario/src/useCases/usuario/db/find-all.user-users.use-cases.js
@@ -1,7 +1,7 @@
 const findAllUserUsers = (findAllUserUsersQuery, findOneUserQuery) => async (principalId) => {
   const query = { principalId };
 
-  const userUsers = await findAllUserUsersQuery({ query });
+  const userUsers = await findAllUserUsersQuery({}, { query });
 
   const usersDetail = await Promise.all(userUsers.map(async (userUser) => {
     const include = [{ association: 'persona' }, { association: 'rol' }];
