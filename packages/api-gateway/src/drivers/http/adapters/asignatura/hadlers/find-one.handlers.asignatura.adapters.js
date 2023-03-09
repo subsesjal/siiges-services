575e642e3f9a8c6dd14cefc2110ce02ddd85eae7
Lau1980 <laura.jaramillo@jalisco.gob.mx>
2023-03-09 16:43:56 -0600

    feat: findOneAsignatura

---
+++ packages/api-gateway/src/drivers/http/adapters/asignatura/hadlers/find-one.handlers.asignatura.adapters.js
@@ -0,0 +1,20 @@
+const { Logger } = require('@siiges-services/shared');
+const errorHandler = require('../../../utils/errorHandler');
+
+async function findOneAsignatura(req, reply) {
+  try {
+    const { asignaturaId } = req.params;
+
+    Logger.info(`[asignaturas]: Getting institución ${asignaturaId}`);
+    const asignatura = await this.institucionServices.findOneAsignatura({ id: asignaturaId });
+
+    return reply
+      .code(200)
+      .header('Content-Type', 'application/json; charset=utf-8')
+      .send({ data: asignatura });
+  } catch (error) {
+    return errorHandler(error, reply);
+  }
+}
+
+module.exports = findOneAsignatura;
