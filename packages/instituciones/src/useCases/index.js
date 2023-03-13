fe5da0f708ad6d19a817e6a3034eb4592e2650d8
Lau1980 <laura.jaramillo@jalisco.gob.mx>
2023-03-13 13:28:07 -0600

    feat: findOneAsignatura

--- packages/instituciones/src/useCases/index.js
+++ packages/instituciones/src/useCases/index.js
@@ -17,8 +17,7 @@ const {
   createDirectorQuery,
   updateDirectorQuery,
   updatePersonaQuery,
-  findOneAsignaturaQuery,
-} = require('../adapters/db');
+ } = require('../adapters/db');
 
 const findAllInstituciones = require('./db/instituciones/find-all.instituciones.use-cases');
 const findOneInstitucion = require('./db/instituciones/find-one.instituciones.use-cases');
@@ -38,7 +37,6 @@ const deleteRatificacion = require('./db/ratificaciones/delete.ratificaciones.us
 const createDirectorPlantel = require('./db/planteles/create.director-plantel.use-cases');
 const updateDirectorPlantel = require('./db/planteles/update.director-plantel.use-cases');
 const findAllMunicipios = require('./db/municipios/find-all.municipios.use-cases');
-const findOneAsignatura = require('./db/asignatura/find-one.asignatura.use-case');
 
 module.exports = {
   findAllInstituciones: findAllInstituciones(findAllInstitucionesQuery),
@@ -86,5 +84,4 @@ module.exports = {
     updateDirectorQuery,
   ),
   findAllMunicipios: findAllMunicipios(findAllMunicipiosQuery),
-  findOneAsignatura: findOneAsignatura(findOneAsignaturaQuery),
 };
