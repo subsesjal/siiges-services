a1c64ef96ed824476caee2ecda217b1a44a71841
Lau1980 <laura.jaramillo@jalisco.gob.mx>
2023-03-13 13:44:34 -0600

    feat: findOneAsignatura

--- packages/solicitudes/src/use-cases/db/asignaturas/index.js
+++ packages/solicitudes/src/use-cases/db/asignaturas/index.js
@@ -1,10 +1,13 @@
 const { asignaturas } = require('../../../adapters/db');
 
 const createAsignaturaPrograma = require('./create.asignatura.use-cases');
+const findOneAsignatura = require('./find-one.asignatura.use-cases');
 
 module.exports = {
   createAsignaturaPrograma: createAsignaturaPrograma(
     asignaturas.findProgramaQuery,
     asignaturas.createAsignaturaProgramaQuery,
+    asignaturas.findOneAsignaturaQuery,
+    findOneAsignatura,
   ),
 };
