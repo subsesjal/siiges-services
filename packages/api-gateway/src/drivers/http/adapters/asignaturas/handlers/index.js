2ef19e1b437320d4df6db7ada08ef33952033640
Lau1980 <laura.jaramillo@jalisco.gob.mx>
2023-03-15 16:48:30 -0600

    feat: find asignatura by programa

--- packages/api-gateway/src/drivers/http/adapters/asignaturas/handlers/index.js
+++ packages/api-gateway/src/drivers/http/adapters/asignaturas/handlers/index.js
@@ -1,6 +1,6 @@
 const createAsignaturaPrograma = require('./create.handlers.asignatura-programa.adapters');
 const findOneAsignaturaPrograma = require('./find-one.handlers.asignatura.adapters');
-const findProgramaAsignatura = require('./find-one.handlers.programa-asignatura.adapters');
+const findProgramaAsignatura = require('./find-all.handlers.programa-asignatura.adapters');
 
 module.exports = {
   createAsignaturaPrograma,
