57c32f3e07f64a74b7f3a4264fa89af4f0f141ed
cetherdev <robert.hct@gmail.com>
2023-03-19 16:37:24 -0600

    fix: file name called

--- packages/api-gateway/src/drivers/http/routes/asignaturas/schema/index.js
+++ packages/api-gateway/src/drivers/http/routes/asignaturas/schema/index.js
@@ -1,5 +1,5 @@
 const createAsignaturaSchema = require('./create.asignatura-programa.schema');
-const findOneAsignaturaSchema = require('./find.one.asignatura.schema');
+const findOneAsignaturaSchema = require('./find-one.asignatura.schema');
 const findProgramaAsignaturasSchema = require('./find.programa-asignaturas.schema');
 
 module.exports = {
