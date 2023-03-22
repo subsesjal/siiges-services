934721fc7ef5ddcca22ea86b8e13675e398930ce
Lau1980 <laura.jaramillo@jalisco.gob.mx>
2023-03-22 13:49:27 -0600

    feat(docente): update docentes

--- packages/solicitudes/src/use-cases/db/docentes/update.docente.use-cases.js
+++ packages/solicitudes/src/use-cases/db/docentes/update.docente.use-cases.js
@@ -1,9 +1,9 @@
 const { Logger } = require('@siiges-services/shared');
 
 const update = (updateQuery) => async (identifierObj, changes) => {
-  Logger.info('[docente/update]: Updating representante');
+  Logger.info('[docente/update]: Updating docente');
   const updatedDocente = await updateQuery(identifierObj, changes);
-  Logger.info('[docente/updated] A representante was updated');
+  Logger.info('[docente/updated] A docente was updated');
 
   return updatedDocente;
 };
