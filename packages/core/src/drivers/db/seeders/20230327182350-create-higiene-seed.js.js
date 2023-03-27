03aba3c242e40fbd2897a2be8d7343b2375de993
Lau1980 <laura.jaramillo@jalisco.gob.mx>
2023-03-27 12:46:17 -0600

    feat(higiene): created table higiene

---
+++ packages/core/src/drivers/db/seeders/20230327182350-create-higiene-seed.js.js
@@ -0,0 +1,20 @@
+const csvToJson = require('convert-csv-to-json');
+const path = require('path');
+
+const { HIGIENE_TABLE } = require('../models/higiene');
+
+const higieneCSV = path.join(__dirname, '../CSVFiles/higiene.csv');
+
+module.exports = {
+  async up(queryInterface) {
+    const higieneJson = await csvToJson
+      .fieldDelimiter(',')
+      .getJsonFromCsv(higieneCSV);
+
+    await queryInterface.bulkInsert(HIGIENE_TABLE, higieneJson, {});
+  },
+
+  async down(queryInterface) {
+    await queryInterface.bulkDelete(HIGIENE_TABLE, null, {});
+  },
+};
