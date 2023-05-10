a1569fc5d4b2a166a3c64e5ff486efd9e90ab93a
TaquitosDorados <62762581+TaquitosDorados@users.noreply.github.com>
2023-05-10 15:44:27 -0600

    fix(inspeccion): use-cases cambiado para que cheque las llaves foraneas

--- packages/inspecciones/src/use-cases/db/inspecciones/create.inspeccion-inspeccion-preguntas.use-cases.js
+++ packages/inspecciones/src/use-cases/db/inspecciones/create.inspeccion-inspeccion-preguntas.use-cases.js
@@ -2,8 +2,38 @@ const { Logger } = require('@siiges-services/shared');
 
 const createInspeccionInspeccionPreguntas = (
   createInspeccionInspeccionPreguntasQuery,
+  //findOneInspeccionQuery,
+  //findOneInspeccionPreguntaQuery,
 ) => async (data) => {
-  Logger.info('[inspeccion/preguntas]: Creating inspeccion-preguntas');
+  Logger.info('[inspeccion/inspeccion/preguntas]: Creating inspeccion-preguntas');
+
+  /* -----------Quiza funcione cuando existan los Querys necesarios
+
+  const newInspeccionInspeccionPreguntaArray = [];
+  await Promise.all(
+    data.inspeccionId.map(async (inspeccionId) => {
+      const inspeccion = await findOneInspeccionQuery({
+        id: inspeccionId,
+      });
+      if(inspeccion) {
+        data.inspeccionPreguntaId.map(async (inspeccionPregunta) => {
+          const pregunta = await findOneInspeccionPreguntaQuery({
+            id: inspeccionPregunta,
+          });
+          if(pregunta) {
+            const newInspeccionInspeccionPregunta = await createInspeccionInspeccionPreguntasQuery({
+              inspeccionId: inspeccionId,
+              inspeccionPreguntaId: inspeccionPregunta,
+              ...data,
+            });
+            newInspeccionInspeccionPreguntaArray.push(newInspeccionInspeccionPregunta);
+          }
+        }),
+      }
+    }),
+  );*/
+
+  //Esto dara error por las llaves foraneas
   const inspeccion = await createInspeccionInspeccionPreguntasQuery(data);
   return inspeccion;
 };
