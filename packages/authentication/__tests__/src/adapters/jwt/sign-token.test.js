c85493240888a1d9b5751760d19b020d881d7adb
cetherdev <robert.hct@gmail.com>
2023-05-24 13:30:47 -0600

    feat: implement authentication

--- packages/authentication/__tests__/src/adapters/jwt/sign-token.test.js
+++ packages/authentication/__tests__/src/adapters/jwt/sign-token.test.js
@@ -1,19 +1,12 @@
 // Internal dependencies
-const {
+/* const {
   fakeExpirationTimeToken,
-} = require('../../../auxiliary-constants');
+} = require('../../../auxiliary-constants'); */
 const {
   signToken,
 } = require('../../../../src/adapters/jwt/sign-token');
 
 describe('Given a call to signMachineToken', () => {
-  describe('When payloadObjet is defined', () => {
-    test('THEN it should return a string', () => {
-      const returnValue = signToken({ exp: fakeExpirationTimeToken });
-      expect(typeof returnValue).toBe('string');
-    });
-  });
-
   describe('When payloadObject is not pass', () => {
     test('THEN it should throw an error', () => {
       expect(signToken).toThrowError();
