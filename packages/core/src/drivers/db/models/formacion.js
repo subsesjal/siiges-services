1f7d8bd279eb6c819cebd673e8b077af5b3a85e9
Daniel Calamateo <dcalamateo@bapcorporativo.com>
2023-09-27 13:06:41 -0600

    ADD[Formacion]: create models and migrations

---
+++ packages/core/src/drivers/db/models/formacion.js
@@ -0,0 +1,71 @@
+const { DataTypes, Model, Sequelize } = require('sequelize');
+const { NIVEL_TABLE } = require('./nivel');
+
+const FORMACION_TABLE = 'formaciones';
+const FormacionSchema = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  nivelId: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    field: 'nivel_id',
+    references: {
+      model: NIVEL_TABLE,
+      key: 'id',
+    },
+  },
+  nombre: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  institucion: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  descripcion: {
+    type: DataTypes.STRING,
+  },
+  fechaGraduado: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  createdAt: {
+    allowNull: false,
+    type: DataTypes.DATE,
+    field: 'created_at',
+    defaultValue: Sequelize.NOW,
+  },
+  updatedAt: {
+    allowNull: true,
+    type: DataTypes.DATE,
+    field: 'updated_at',
+    defaultValue: null,
+  },
+  deletedAt: {
+    allowNull: true,
+    type: DataTypes.DATE,
+    field: 'deleted_at',
+    defaultValue: null,
+  },
+};
+
+class Formacion extends Model {
+  static associations(models) {
+    this.belongsTo(models.Nivel, { as: 'nivel' });
+  }
+
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: FORMACION_TABLE,
+      modelName: 'Formacion',
+      timestamps: false,
+    };
+  }
+}
+
+module.exports = { FORMACION_TABLE, FormacionSchema, Formacion };
---
+++ packages/core/src/drivers/db/models/formacionRector.js
@@ -0,0 +1,67 @@
+const { DataTypes, Model, Sequelize } = require('sequelize');
+const { RECTOR_TABLE } = require('./rector');
+const { FORMACION_TABLE } = require('./formacion');
+
+const FORMACION_RECTOR_TABLE = 'formaciones_rectores';
+const FormacionRectorSchema = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  rectorId: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    field: 'rector_id',
+    references: {
+      model: RECTOR_TABLE,
+      key: 'id',
+    },
+  },
+  formacionId: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    field: 'formacion_id',
+    references: {
+      model: FORMACION_TABLE,
+      key: 'id',
+    },
+  },
+  createdAt: {
+    allowNull: false,
+    type: DataTypes.DATE,
+    field: 'created_at',
+    defaultValue: Sequelize.NOW,
+  },
+  updatedAt: {
+    allowNull: true,
+    type: DataTypes.DATE,
+    field: 'updated_at',
+    defaultValue: null,
+  },
+  deletedAt: {
+    allowNull: true,
+    type: DataTypes.DATE,
+    field: 'deleted_at',
+    defaultValue: null,
+  },
+};
+
+class FormacionRector extends Model {
+  static associations(models) {
+    this.belongsTo(models.Formacion, { as: 'formacion' });
+    this.belongsTo(models.Rector, { as: 'rector' });
+  }
+
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: FORMACION_RECTOR_TABLE,
+      modelName: 'FormacionRector',
+      timestamps: false,
+    };
+  }
+}
+
+module.exports = { FORMACION_RECTOR_TABLE, FormacionRectorSchema, FormacionRector };
