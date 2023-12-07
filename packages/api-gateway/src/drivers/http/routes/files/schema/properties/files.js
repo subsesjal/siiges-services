98cfecb15b3190722e26386254f2d4e5df47831f
Calamateo <dany.calamateo@gmail.com>
2023-12-07 12:28:37 -0600

    ADD[Files]: Create schemas, fix handlers

---
+++ packages/api-gateway/src/drivers/http/routes/files/schema/properties/files.js
@@ -0,0 +1,10 @@
+const files = {
+  tipoEntidadId: { type: 'integer' },
+  entidadId: { type: 'integer' },
+  tipoDocumentoId: { type: 'integer' },
+  nombre: { type: 'string' },
+  ubicacion: { type: 'string' },
+  url: { type: 'string' },
+};
+
+module.exports = { files };
