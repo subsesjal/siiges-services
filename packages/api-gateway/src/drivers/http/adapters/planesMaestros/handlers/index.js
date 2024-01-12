f61972d2ca6b4be5659afac87852afda15d5818d
Calamateo <dany.calamateo@gmail.com>
2024-01-12 16:49:29 -0600

    ADD[PlanMaestro]: Create adapters for Plan Maestro

---
+++ packages/api-gateway/src/drivers/http/adapters/planesMaestros/handlers/index.js
@@ -0,0 +1,13 @@
+const { createPlanMaestro } = require('./create.handlers.plan-maestro.adapters');
+const { createResponsables } = require('./create.handlers.representante.adapters');
+const { createDatosDeProyecto } = require('./create.handlers.datos-de-proyecto.adapters');
+const { findOneDatosPlanMaestro } = require('./find-one.handlers.datos-plan-maestro.adapters');
+const { findOneResponsablesPlanMaestro } = require('./find-one.handlers.responsables-plan-maestro.adapters');
+
+module.exports = {
+  createPlanMaestro,
+  createResponsables,
+  createDatosDeProyecto,
+  findOneDatosPlanMaestro,
+  findOneResponsablesPlanMaestro,
+};
