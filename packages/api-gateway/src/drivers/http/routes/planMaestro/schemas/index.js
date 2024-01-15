27d29dca16902c90eb94f2152366e8f296961b32
Calamateo <dany.calamateo@gmail.com>
2024-01-15 12:00:11 -0600

    ADD[PlanMaestro]: create schema findAllPlanMaestro

--- packages/api-gateway/src/drivers/http/routes/planMaestro/schemas/index.js
+++ packages/api-gateway/src/drivers/http/routes/planMaestro/schemas/index.js
@@ -3,6 +3,7 @@ const { createPlanMaestroSchema } = require('./create.planes-maestros.schema');
 const { createResponsablesSchema } = require('./create.responsables.schema');
 const { createDatosDeProyectoSchema } = require('./create.datos.schema');
 const { findOneResponsablesSchema } = require('./find-one.responsables.schema');
+const { findAllPlanMaestroSchema } = require('./find-all.plan-maestro.schema');
 
 module.exports = {
   createPlanMaestroSchema,
@@ -10,4 +11,5 @@ module.exports = {
   createDatosDeProyectoSchema,
   findOneDatosDeProyectoSchema,
   findOneResponsablesSchema,
+  findAllPlanMaestroSchema,
 };
