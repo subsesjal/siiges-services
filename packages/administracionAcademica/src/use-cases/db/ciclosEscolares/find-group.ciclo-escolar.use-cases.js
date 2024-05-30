0c255fbabe3df0a250313f0218770ecca71f491d
calamateo <dany.calamateo@gmail.com>
2024-05-30 12:04:38 -0600

    feat: update findGroupCicloEscolarQuery to use programaId in find-group.ciclo-escolar.use-cases.js

--- packages/administracionAcademica/src/use-cases/db/ciclosEscolares/find-group.ciclo-escolar.use-cases.js
+++ packages/administracionAcademica/src/use-cases/db/ciclosEscolares/find-group.ciclo-escolar.use-cases.js
@@ -1,7 +1,7 @@
 const { checkers } = require('@siiges-services/shared');
 
 const findGroupCicloEscolar = (findGroupCicloEscolarQuery) => async (identifierObj) => {
-  const cicloEscolar = await findGroupCicloEscolarQuery(identifierObj);
+  const cicloEscolar = await findGroupCicloEscolarQuery({ programaId: identifierObj });
   checkers.throwErrorIfDataIsFalsy(!!cicloEscolar.length, 'Ciclo Escolar', identifierObj.programaId);
 
   return cicloEscolar;
