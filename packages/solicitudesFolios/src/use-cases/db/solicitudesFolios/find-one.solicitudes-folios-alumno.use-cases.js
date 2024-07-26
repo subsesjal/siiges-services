7e64b385d86bd5aea6a650902fae6bf44d9f649a
Dylan Sanchez <dylansanchez2902@gmail.com>
2024-07-26 16:04:29 -0600

    Corrección solicitud-alumnos-usescases comentarios marcados

--- packages/solicitudesFolios/src/use-cases/db/solicitudesFolios/find-one.solicitudes-folios-alumno.use-cases.js
+++ packages/solicitudesFolios/src/use-cases/db/solicitudesFolios/find-one.solicitudes-folios-alumno.use-cases.js
@@ -1,25 +1,19 @@
 const { checkers } = require('@siiges-services/shared');
 
 const findOneSolicitudFolioAlumno = (findOneSolicitudFolioAlumnoQuery) => async ({ id }) => {
-  try {
-    if (!id) throw new Error('ID is required');
+  const include = [
+    {
+      association: 'alumno',
+      include: [
+        { association: 'persona' },
+      ],
+    },
+  ];
 
-    const include = [
-      {
-        association: 'alumno',
-        include: [
-          { association: 'persona' },
-        ],
-      },
-    ];
+  const alumno = await findOneSolicitudFolioAlumnoQuery({ id }, { include });
+  checkers.throwErrorIfDataIsFalsy(alumno, 'solicitudes-folios-alumnos', id);
 
-    const alumno = await findOneSolicitudFolioAlumnoQuery({ id }, { include });
-    checkers.throwErrorIfDataIsFalsy(alumno, 'solicitudes-folios-alumnos', id);
-
-    return alumno;
-  } catch (error) {
-    throw new Error(`Error finding alumno: ${error.message}`);
-  }
+  return alumno;
 };
 
 module.exports = findOneSolicitudFolioAlumno;
