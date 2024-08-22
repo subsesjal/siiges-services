63bfaaee5d64c4271e7f1884d65e8b3cd6bdc0a7
Dylan Sanchez <dylansanchez2902@gmail.com>
2024-08-22 15:03:42 -0600

    SDT-644: Ajustando endpoint con datos de plantel, domicilio e institucion

--- packages/api-gateway/src/drivers/http/routes/solicitudesFolios/schema/find-all.solicitudes-folio.schema.js
+++ packages/api-gateway/src/drivers/http/routes/solicitudesFolios/schema/find-all.solicitudes-folio.schema.js
@@ -4,6 +4,11 @@ const { tipoSolicitudFolio } = require('./properties/tipoSolicitudFolio');
 const { programa } = require('../../solicitudes/schema/properties/programa');
 const { estatusSolicitudFolio } = require('./properties/estatusSolicitudFolio');
 const { responseProperties } = require('./properties/responseProperties');
+const { institucion } = require('../../instituciones/schema/properties/institucion');
+const { plantel } = require('../../instituciones/schema/properties/plantel');
+const { domicilio } = require('../../usuarios/schema/properties/domicilio');
+const { estado } = require('../../usuarios/schema/properties/estado');
+const { municipio } = require('../../usuarios/schema/properties/municipio');
 
 const findAllSolicitudesProgramasSchema = {
   tags: ['Solicitudes Folios'],
@@ -65,6 +70,46 @@ const findAllSolicitudesProgramasSchema = {
                   id: { type: 'integer' },
                   ...programa,
                   ...responseProperties,
+                  plantel: {
+                    type: 'object',
+                    properties: {
+                      id: { type: 'integer' },
+                      ...plantel,
+                      ...responseProperties,
+                      domicilio: {
+                        type: 'object',
+                        properties: {
+                          id: { type: 'integer' },
+                          ...domicilio,
+                          ...responseProperties,
+                          municipio: {
+                            type: 'object',
+                            properties: {
+                              id: { type: 'integer' },
+                              ...municipio,
+                              ...responseProperties,
+                            },
+                          },
+                          estado: {
+                            type: 'object',
+                            properties: {
+                              id: { type: 'integer' },
+                              ...estado,
+                              ...responseProperties,
+                            },
+                          },
+                        },
+                      },
+                      institucion: {
+                        type: 'object',
+                        properties: {
+                          id: { type: 'integer' },
+                          ...institucion,
+                          ...responseProperties,
+                        },
+                      },
+                    },
+                  },
                 },
               },
             },
