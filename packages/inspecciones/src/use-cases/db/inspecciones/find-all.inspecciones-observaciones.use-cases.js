eb816d82519cf4461ba4dc2ac206bc7391bab81a
Dylan Sanchez <dylansanchez2902@gmail.com>
2024-09-12 16:22:04 -0600

    Checker removido

--- packages/inspecciones/src/use-cases/db/inspecciones/find-all.inspecciones-observaciones.use-cases.js
+++ packages/inspecciones/src/use-cases/db/inspecciones/find-all.inspecciones-observaciones.use-cases.js
@@ -1,10 +1,7 @@
-const { checkers } = require('@siiges-services/shared');
-
 const findAllInspeccionObservaciones = (
   findAllObservacionesQuery,
 ) => async ({ inspeccionId }) => {
   const inspeccionesObservacion = await findAllObservacionesQuery({ inspeccionId });
-  checkers.throwErrorIfDataIsFalsy(inspeccionesObservacion, 'Inspecciones-preguntas', inspeccionId);
   return inspeccionesObservacion;
 };
 
