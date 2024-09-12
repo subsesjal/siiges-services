d261c94a3e78e318d7b6fa93cb47ae829c5fa06c
cetherdev <robert.hct@gmail.com>
2024-09-11 23:14:04 -0600

    fix e2e tests

--- __test__/usuarios.e2e.js
+++ __test__/usuarios.e2e.js
@@ -29,61 +29,70 @@ describe('test for users', () => {
     api = supertest(await upServer());
     await upSeed();
   });
-  describe('[POST] /api/v1/usuarios', () => {
-    it('should new user', async () => {
-      const { body, statusCode } = await api.post('/api/v1/usuarios')
-        .send(createUser)
-        .set(apiKey);
-      id = body.data.id;
-      const data = await models.Usuario.findByPk(id);
-      expect(statusCode).toEqual(statusCodeName.created);
-      expect(body.data.usuario).toEqual(data.usuario);
-    });
-    it('should exists user', async () => {
-      const { statusCode, body } = await api.post('/api/v1/usuarios')
-        .send(createUser)
-        .set(apiKey);
-      expect(statusCode).toEqual(statusCodeName.conflict);
-      expect(body.message).toEqual(`User ${createUser.usuario} already exists`);
-    });
-    it('should values name null', async () => {
-      const { body } = await api.post('/api/v1/usuarios')
-        .send(createUserWithoutPerson)
-        .set(apiKey);
-      expect(body.data.persona.nombre).toEqual('SIN DATO');
-    });
-    it('should error password user', async () => {
-      const { statusCode, body } = await api.post('/api/v1/usuarios')
-        .send(errorPasswordUserCreate)
-        .set(apiKey);
-      expect(statusCode).toEqual(statusCodeName.badRequest);
-      expect(body.message).toEqual('body/contrasena must match pattern "^(?!.* )(?=.*[a-z])(?=.*[A-Z])(?=.*?[0-9])(?=.*[@$!%*?&./])[A-Za-z0-9@$!%*?&./]{8,25}$"');
-    });
-    it('should error create user', async () => {
-      const { statusCode, body } = await api.post('/api/v1/usuarios')
-        .send(errorUserCreate)
-        .set(apiKey);
-      expect(statusCode).toEqual(statusCodeName.badRequest);
-      expect(body.message).toEqual('body/usuario must NOT have fewer than 3 characters');
-    });
-  });
 
   describe('[POST] /api/v1/auth/login', () => {
     it('should login', async () => {
-      const { body, statusCode } = await api.post('/api/v1/auth/login')
+      const { body, statusCode } = await api.post('/api/v1/public/auth/login')
         .send(loginUser)
         .set(apiKey);
+
       token = body.token;
       headers = updateToken(token);
       expect(statusCode).toEqual(statusCodeName.ok);
       expect(body.data.usuario).toEqual(loginUser.usuario);
     });
+
     it('should Unauthorized access', async () => {
-      const { statusCode, body } = await api.post('/api/v1/auth/login')
+      const { statusCode, body } = await api.post('/api/v1/public/auth/login')
         .send(userPasswordIncorrect)
         .set(apiKey);
+
       expect(statusCode).toEqual(statusCodeName.unauthorized);
       expect(body.message).toEqual('Unauthorized');
+    });
+  });
+
+  describe('[POST] /api/v1/usuarios', () => {
+    it('should new user', async () => {
+      const { body, statusCode } = await api.post('/api/v1/usuarios')
+        .send(createUser)
+        .set(headers);
+
+      id = body.data.id;
+      const data = await models.Usuario.findByPk(id);
+      expect(statusCode).toEqual(statusCodeName.created);
+      expect(body.data.usuario).toEqual(data.usuario);
+    });
+
+    it('should exists user', async () => {
+      const { statusCode, body } = await api.post('/api/v1/usuarios')
+        .send(createUser)
+        .set(headers);
+      expect(statusCode).toEqual(statusCodeName.conflict);
+      expect(body.message).toEqual(`User ${createUser.usuario} already exists`);
+    });
+
+    it('should values name null', async () => {
+      const { body } = await api.post('/api/v1/usuarios')
+        .send(createUserWithoutPerson)
+        .set(headers);
+      expect(body.data.persona.nombre).toEqual('SIN DATO');
+    });
+
+    it('should error password user', async () => {
+      const { statusCode, body } = await api.post('/api/v1/usuarios')
+        .send(errorPasswordUserCreate)
+        .set(headers);
+      expect(statusCode).toEqual(statusCodeName.badRequest);
+      expect(body.message).toEqual('body/contrasena must match pattern "^(?!.* )(?=.*[a-z])(?=.*[A-Z])(?=.*?[0-9])(?=.*[@$!%*?&./])[A-Za-z0-9@$!%*?&./]{8,25}$"');
+    });
+
+    it('should error create user', async () => {
+      const { statusCode, body } = await api.post('/api/v1/usuarios')
+        .send(errorUserCreate)
+        .set(headers);
+      expect(statusCode).toEqual(statusCodeName.badRequest);
+      expect(body.message).toEqual('body/usuario must NOT have fewer than 3 characters');
     });
   });
 
@@ -103,13 +112,6 @@ describe('test for users', () => {
   });
 
   describe('[GET] /api/v1/usuarios/:{id}', () => {
-    // it('should a user', async () => {
-    //   const { body, statusCode } = await api.get(`/api/v1/usuarios/${id}`)
-    //     .set(headers);
-    //   expect(body.data.id).toEqual(id);
-    //   expect(body.data.usuario).toEqual(loginUser.usuario);
-    //   expect(statusCode).toEqual(statusCodeName.ok);
-    // });
     it('should user not found', async () => {
       const { body: error, statusCode } = await api.get('/api/v1/usuarios/-1').set(headers);
       expect(statusCode).toEqual(statusCodeName.notFound);
