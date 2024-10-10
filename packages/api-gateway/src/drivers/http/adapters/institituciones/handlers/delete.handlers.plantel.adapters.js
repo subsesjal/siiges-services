725a9bc7e00dd93e9132d900c4d0f6292a330137
Dylan Sanchez <dylansanchez2902@gmail.com>
2024-10-10 12:50:53 -0600

    SDT-740: Enviar notificaciones: crear solicitud, eliminar solicitud, crear plantel y eliminar plantel

--- packages/api-gateway/src/drivers/http/adapters/institituciones/handlers/delete.handlers.plantel.adapters.js
+++ packages/api-gateway/src/drivers/http/adapters/institituciones/handlers/delete.handlers.plantel.adapters.js
@@ -1,12 +1,29 @@
 const { Logger } = require('@siiges-services/shared');
 const errorHandler = require('../../../utils/errorHandler');
 
+async function sendEmailNotification(notificacionServices, emailDestination, idUser, userName) {
+  Logger.info('[notification]: Sending notification');
+  await notificacionServices.sendNotificationEmail({
+    usuarioId: idUser,
+    email: emailDestination,
+    asunto: 'SIIGES: Confirmación para Eliminación de Plantel',
+    template: 'deletePlantel',
+    params: {
+      user: userName,
+    },
+  });
+}
+
 async function deletePlantel(req, reply) {
   try {
     const { institucionId, plantelId } = req.params;
-
+    const usuarioId = req.user.userPayload.id;
+    const usuario = await this.usuarioServices.findOneUser({ id: usuarioId });
+    const { correo } = usuario.dataValues;
+    const { id } = usuario.dataValues;
+    const nombreUsuario = usuario.dataValues.usuario;
     Logger.info(`[instituciones]: Deleting plantel ${plantelId}`);
-
+    sendEmailNotification(this.notificacionServices, correo, id, nombreUsuario);
     const plantel = await this.institucionServices.deletePlantel({
       institucionId,
       plantelId,
--- packages/api-gateway/src/drivers/http/adapters/solicitudes/handlers/delete.handlers.solicitud.adapters.js
+++ packages/api-gateway/src/drivers/http/adapters/solicitudes/handlers/delete.handlers.solicitud.adapters.js
@@ -1,13 +1,35 @@
 const { Logger } = require('@siiges-services/shared');
 const errorHandler = require('../../../utils/errorHandler');
 
+async function sendEmailNotification(notificacionServices, emailDestination, idUser, userName) {
+  Logger.info('[notification]: Sending notification');
+  await notificacionServices.sendNotificationEmail({
+    usuarioId: idUser,
+    email: emailDestination,
+    asunto: 'SIIGES: Confirmación de Eliminación de Solicitud',
+    template: 'deleteSolicitud',
+    params: {
+      user: userName,
+    },
+  });
+}
+
 async function deleteSolicitud(req, reply) {
   try {
     const { solicitudId } = req.params;
+    const solicitudData = await this.solicitudServices.findOneSolicitudPrograma(
+      { id: solicitudId },
+    );
     Logger.info(`[Solicitud]: Deleting solicitud with id: ${solicitudId}`);
     const solicitud = await this.solicitudServices.deleteSolicitud({
       id: solicitudId,
     });
+    const { usuarioId } = solicitudData.dataValues;
+    const usuario = await this.usuarioServices.findOneUser({ id: usuarioId });
+    const { correo } = usuario.dataValues;
+    const { id } = usuario.dataValues;
+    const nombreUsuario = usuario.dataValues.usuario;
+    sendEmailNotification(this.notificacionServices, correo, id, nombreUsuario);
     return reply
       .code(200)
       .header('Content-Type', 'application/json; charset=utf-8')
