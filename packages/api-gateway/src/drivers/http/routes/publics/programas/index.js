b0f535927a872a99996c4af7d7ef9d5a14bc9797
Zenitramhgx <zenitramhgx15@hotmail.com>
2024-10-11 23:41:05 -0600

    SDT-754: fix: Public Endpoint programas

--- packages/api-gateway/src/drivers/http/routes/publics/programas/index.js
+++ packages/api-gateway/src/drivers/http/routes/publics/programas/index.js
@@ -1,9 +1,18 @@
 const { programasAdapter } = require('../../../adapters');
 const {
   findPlantelProgramasSchema,
+  findAllProgramasSchema,
 } = require('../../privates/programas/schema');
 
 async function programasRouter(fastify, opts, next) {
+  await fastify.get(
+    '/',
+    {
+      schema: findAllProgramasSchema,
+    },
+    programasAdapter.findAllProgramas,
+  );
+
   await fastify.get(
     '/planteles/:plantelId',
     {
