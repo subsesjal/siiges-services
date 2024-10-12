d20a06538e04c3802c4956b1d09f11b37fe54ee3
Dylan Sanchez <dylansanchez2902@gmail.com>
2024-10-12 05:25:59 -0600

    Conflicto de iteración

--- packages/api-gateway/src/drivers/http/adapters/solicitudes/handlers/create.handlers.equivalencias.adapters.js
+++ packages/api-gateway/src/drivers/http/adapters/solicitudes/handlers/create.handlers.equivalencias.adapters.js
@@ -1,38 +1,16 @@
 const { Logger } = require('@siiges-services/shared');
 const boom = require('@hapi/boom');
 const errorHandler = require('../../../utils/errorHandler');
-const FILES_MAPPING = [
-  'ARCHIVO_CURP',
-  'ARCHIVO_NACIMIENTO',
-  'ARCHIVO_CERTIFICADO',
-  'PROGRAMA_AUTORIZADO',
-  'IDENTIFICACION_OFICIAL',
-  'RESOLUCION',
-  'ANTECEDENTE_ACADEMICO',
-  'PROPUESTA',
-  'COMPROBANTE_PAGO_TRAMITE',
-];
 
 async function createEquivalencia(req, reply) {
   try {
-    // Acceder al archivo desde el campo "FILE" y a los datos desde "DATA"
-    const fotografia = req.body.FIRMA_REPRESENTANTE; // Aquí accedemos directamente a FILE
-    const dataField = req.body.DATA; // Accedemos al contenido de DATA
-    //console.log(req.body);
-    // if (!fotografia || !dataField) {
-    //   return reply
-    //     .code(400)
-    //     .send({ message: 'Datos insuficientes: asegúrate de que se incluyan el archivo y los datos.' });
-    // }
-
-    // Parsear el JSON de DATA.value
-    const data2 = await req.saveRequestFiles();
-    console.log(data2);
+    const fotografia = req.body.FIRMA_REPRESENTANTE;
+    const dataField = req.body.DATA;
+    const fileData = await req.saveRequestFiles();
     let data;
     try {
-      data = JSON.parse(dataField.value); // Accedemos al valor del campo DATA
+      data = JSON.parse(dataField.value);
     } catch (error) {
-      console.error('Error al parsear los datos JSON:', error);
       return reply
         .code(400)
         .send({ message: 'Error al parsear los datos JSON en el campo DATA.' });
@@ -40,38 +18,30 @@ async function createEquivalencia(req, reply) {
 
     Logger.info('[equivalencia]: Creating equivalencia');
 
-    // Llamamos al servicio con los datos parseados y el archivo
     const newEquivalencia = await this.solicitudServices.createEquivalencia(data, fotografia);
     const { id } = newEquivalencia.dataValues;
-
-    // const filesSaved = FILES_MAPPING.map((fileName) => {
-    //   console.log(fileName);
-    //   uploadFile(req, fileName, id);
-    // });
     const fileKeys = Object.keys(req.body).filter((key) => key !== 'DATA');
-    fileKeys.forEach(async (key) => {
-      console.log(key);
-      const archivoAdjunto = data2.find((files) => files.fieldname === key);
+    await fileKeys.reduce(async (prevPromise, key) => {
+      await prevPromise;
+      const archivoAdjunto = fileData.find((files) => files.fieldname === key);
       if (!archivoAdjunto) {
         throw boom.badRequest('Archivo adjunto requerido.');
       }
+
       const dataFile = {
         tipoEntidad: 'SOLICITUD_REV_EQUIV',
         entidadId: id,
         tipoDocumento: key,
       };
 
-      this.filesServices.uploadFile(dataFile, archivoAdjunto);
-    });
-    // const data2 = await req.saveRequestFiles();
-    // const fileKeys = Object.keys(req.body).filter((key) => key !== 'DATA');
+      await this.filesServices.uploadFile(dataFile, archivoAdjunto);
+    }, Promise.resolve());
 
     return reply
       .code(201)
       .header('Content-Type', 'application/json; charset=utf-8')
       .send({ data: newEquivalencia });
   } catch (error) {
-    console.error(error);
     return errorHandler(error, reply);
   }
 }
