e2ac88d3a3d36fdec2cd09aa7ec495f24868f78b
Dylan Sanchez <dylansanchez2902@gmail.com>
2024-10-14 16:53:56 -0600

    Mapping agregado

--- packages/api-gateway/src/drivers/http/adapters/solicitudesRevEquiv/handlers/create.handlers.equivalencias.adapters.js
+++ packages/api-gateway/src/drivers/http/adapters/solicitudesRevEquiv/handlers/create.handlers.equivalencias.adapters.js
@@ -2,12 +2,26 @@ const { Logger } = require('@siiges-services/shared');
 const boom = require('@hapi/boom');
 const errorHandler = require('../../../utils/errorHandler');
 
+const FILE_KEYS_MAPPING = {
+  ARCHIVO_CURP: 'ARCHIVO_CURP',
+  ARCHIVO_NACIMIENTO: 'ARCHIVO_NACIMIENTO',
+  ARCHIVO_CERTIFICADO: 'ARCHIVO_CERTIFICADO',
+  PROGRAMA_AUTORIZADO: 'PROGRAMA_AUTORIZADO',
+  IDENTIFICACION_OFICIAL: 'IDENTIFICACION_OFICIAL',
+  RESOLUCION: 'RESOLUCION',
+  ANTECEDENTE_ACADEMICO: 'ANTECEDENTE_ACADEMICO',
+  PROPUESTA: 'PROPUESTA',
+  COMPROBANTE_PAGO_TRAMITE: 'COMPROBANTE_PAGO_TRAMITE',
+};
+
 async function createEquivalencia(req, reply) {
   try {
     const fotografia = req.body.FIRMA_REPRESENTANTE;
     const dataField = req.body.DATA;
     const fileData = await req.saveRequestFiles();
     let data;
+
+    // Intentar parsear el campo DATA
     try {
       data = JSON.parse(dataField.value);
     } catch (error) {
@@ -20,12 +34,13 @@ async function createEquivalencia(req, reply) {
 
     const newEquivalencia = await this.solicitudServices.createEquivalencia(data, fotografia);
     const { id } = newEquivalencia.dataValues;
-    const fileKeys = Object.keys(req.body).filter((key) => key !== 'DATA');
+    const fileKeys = Object.keys(req.body)
+      .filter((key) => Object.prototype.hasOwnProperty.call(FILE_KEYS_MAPPING, key) && key !== 'DATA');
     await fileKeys.reduce(async (prevPromise, key) => {
       await prevPromise;
       const archivoAdjunto = fileData.find((files) => files.fieldname === key);
       if (!archivoAdjunto) {
-        throw boom.badRequest('Archivo adjunto requerido.');
+        throw boom.badRequest('Archivo adjunto requerido para: ', key);
       }
 
       const dataFile = {
