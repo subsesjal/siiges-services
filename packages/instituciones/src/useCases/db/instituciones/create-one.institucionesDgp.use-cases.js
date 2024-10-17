972a0614971f694434070864d4bd62ef1113c595
Zenitramhgx <zenitramhgx15@hotmail.com>
2024-10-17 08:40:19 -0600

    SDT-771: fix: Actualizar registro institucionDgp.

--- packages/instituciones/src/useCases/db/instituciones/create-one.institucionesDgp.use-cases.js
+++ packages/instituciones/src/useCases/db/instituciones/create-one.institucionesDgp.use-cases.js
@@ -1,22 +1,36 @@
 const createOneInstitucionesDgp = (
   createInstitucionesDgpQuery,
   findInstitucionByIdQuery,
+  findOneInstitucionDgpQuery, // Buscar por claveDgp
+  updateInstitucionDgpQuery
 ) => async (data) => {
   const institucionesDgpPromises = data.map(async (item) => {
     const { institucionId, claveDgp, nombreInstitucionDgp } = item;
 
-    const institucionExists = await findInstitucionByIdQuery({ id: institucionId });
-    if (!institucionExists) return null;
+    const institucionDgpExists = await findOneInstitucionDgpQuery({ claveDgp });
+    if (institucionDgpExists) {
+      const updatedInstitucionDgp = await updateInstitucionDgpQuery(
+        { claveDgp },
+        {
+          institucionId,
+          claveDgp,
+          nombreInstitucionDgp,
+        }
+      );
+      return updatedInstitucionDgp;
+    } else {
+      const institucionExists = await findInstitucionByIdQuery({ id: institucionId });
 
-    const newInstitucionDgp = await createInstitucionesDgpQuery({
-      institucionId,
-      claveDgp,
-      nombreInstitucionDgp,
-    });
+      if (!institucionExists) return null;
+      const newInstitucionDgp = await createInstitucionesDgpQuery({
+        institucionId,
+        claveDgp,
+        nombreInstitucionDgp,
+      });
 
-    return newInstitucionDgp;
+      return newInstitucionDgp;
+    }
   });
-
   const institucionesDgp = await Promise.all(institucionesDgpPromises);
 
   return institucionesDgp;
