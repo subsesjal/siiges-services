79b0ad82be1e05b643ca8976435bae75409f49e8
Arely Rizo <arelyruiz50700@gmail.com>
2025-03-21 11:40:58 -0600

    Modificacion para eliminacion de solicitud Servicio social

--- packages/api-gateway/src/drivers/http/adapters/solicitudesServSoc/handlers/delete.handlers.solicitud-serv-soc.adapters.js
+++ packages/api-gateway/src/drivers/http/adapters/solicitudesServSoc/handlers/delete.handlers.solicitud-serv-soc.adapters.js
@@ -1,17 +1,15 @@
 const { Logger } = require('@siiges-services/shared');
 const errorHandler = require('../../../utils/errorHandler');
 
-const deleteSolicitudServSoc = async (req, reply) => {
+async function deleteSolicitudServSoc(req, reply) {
   try {
     const { solicitudServicioSocialId } = req.params;
 
     Logger.info(`[solicitud-servicio-social]: Deleting solicitud servicio social with id: ${solicitudServicioSocialId}`);
 
-    const SolicitudEliminada = await req.server.solicitudServicioSocialServices.deleteSolicitudServSoc({
+    const SolicitudEliminada = await this.solicitudServicioSocialServices.deleteSolicitudServSoc({
       id: solicitudServicioSocialId,
     });
-
-    Logger.info(`[DELETE Solicitud con id ${solicitudServicioSocialId} eliminada correctamente`);
 
     return reply
       .code(200)
@@ -20,6 +18,6 @@ const deleteSolicitudServSoc = async (req, reply) => {
   } catch (error) {
     return errorHandler(error, reply);
   }
-};
+}
 
 module.exports = { deleteSolicitudServSoc };
