e40e402c565374275fffd0918f56dcd8c86f333d
cetherdev <robert.hct@gmail.com>
2025-03-23 14:55:14 -0600

    SDT-929: update alumno for one solicitud serv social

--- packages/api-gateway/src/drivers/http/adapters/solicitudesServSoc/handlers/index.js
+++ packages/api-gateway/src/drivers/http/adapters/solicitudesServSoc/handlers/index.js
@@ -3,6 +3,7 @@ const { findOneSolicitudServSoc } = require('./find-one.handlers.solicitud-serv-
 const { findAllSolicitudesServSoc } = require('./find-all.handlers.solicitudes-serv-soc.adapters');
 const { updateSolicitudServSoc } = require('./update.handlers.solicitud-serv-soc.adapters');
 const { createSolicitudServSocAlumno } = require('./create.handlers.solicitud-ser-soc-alumno.adapters');
+const { updateSolicitudServSocAlumno } = require('./update.handlers.solicitud-ser-soc-alumno.adapters');
 const { findOneSolicitudServSocAlumno } = require('./find-one.handlers.solicitud-serv-soc-alumno.adapters');
 const { findAllSolicitudesServSocAlumno } = require('./find-all.handlers.solicitudes-serv-soc-alumnos.adapters');
 const { deleteSolicitudServSoc } = require('./delete.handlers.solicitud-serv-soc.adapters');
@@ -15,6 +16,7 @@ module.exports = {
   findAllSolicitudesServSoc,
   updateSolicitudServSoc,
   createSolicitudServSocAlumno,
+  updateSolicitudServSocAlumno,
   findOneSolicitudServSocAlumno,
   findAllSolicitudesServSocAlumno,
   deleteSolicitudServSoc,
