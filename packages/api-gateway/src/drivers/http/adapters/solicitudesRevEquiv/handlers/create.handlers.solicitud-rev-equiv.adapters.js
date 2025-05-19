eb50d79dd2e143ef779dd7661a1075bc7305f937
Arely Rizo <arelyruiz50700@gmail.com>
2025-05-19 16:09:52 -0600

    SDT-1023:Notificaciones en Revalidaciones/Equivalencias

--- packages/api-gateway/src/drivers/http/adapters/solicitudesRevEquiv/handlers/create.handlers.solicitud-rev-equiv.adapters.js
+++ packages/api-gateway/src/drivers/http/adapters/solicitudesRevEquiv/handlers/create.handlers.solicitud-rev-equiv.adapters.js
@@ -50,6 +50,17 @@ async function createEquivalencia(req, reply) {
       await this.filesServices.uploadFile(dataFile, archivoAdjunto);
     }, Promise.resolve());
 
+    // Enviar notificación si hay cambio de estatus
+    this.notificacionServices.sendNotificationEmail({
+      usuarioId: 14,
+      email: 'joel.duran@jalisco.gob.mx',
+      asunto: 'SIIGES: Cambiar de estatus - solicitudRevEquiv',
+      template: 'solicitudRevEquivRecibida',
+      params: {
+        folioSolicitud: newEquivalencia.folioSolicitud,
+      },
+    });
+
     return reply
       .code(200)
       .header('Content-Type', 'application/json; charset=utf-8')
--- packages/api-gateway/src/drivers/http/adapters/solicitudesRevEquiv/handlers/update.handlers.solicitud-rev-equiv.adapters.js
+++ packages/api-gateway/src/drivers/http/adapters/solicitudesRevEquiv/handlers/update.handlers.solicitud-rev-equiv.adapters.js
@@ -1,5 +1,65 @@
 const errorHandler = require('../../../utils/errorHandler');
 
+const FEATURE = {
+  EN_FIRMA: 3,
+  PROCESADA: 4,
+  ATENDER_OBSERVACIONES: 5,
+};
+
+const NOTIFICATION_MAPPING = {
+  [FEATURE.RECIBIDA]: async (processor, solicitud) => {
+    await processor({
+      usuarioId: 14,
+      email: solicitud.interesado.persona.correoPrimario,
+      asunto: `SIIGES: Solicitud en Firma - Folio ${solicitud.folioSolicitud}`,
+      template: 'solicitudRevEquivRecibida',
+      params: {
+        folioSolicitud: solicitud.folioSolicitud,
+      },
+    });
+  },
+  [FEATURE.EN_FIRMA]: async (processor, solicitud) => {
+    await processor({
+      usuarioId: 14,
+      email: solicitud.interesado.persona.correoPrimario,
+      asunto: `SIIGES: Solicitud en Firma - Folio ${solicitud.folioSolicitud}`,
+      template: 'solicitudRevEquivEnFirma',
+      params: {
+        folioSolicitud: solicitud.folioSolicitud,
+      },
+    });
+  },
+  [FEATURE.PROCESADA]: async (processor, solicitud) => {
+    await processor({
+      usuarioId: 14,
+      email: solicitud.interesado.persona.correoPrimario,
+      asunto: `SIIGES: Solicitud Procesada - Folio ${solicitud.folioSolicitud}`,
+      template: 'solicitudRevEquivProcesada',
+      params: {
+        folioSolicitud: solicitud.folioSolicitud,
+      },
+    });
+  },
+  [FEATURE.ATENDER_OBSERVACIONES]: async (processor, solicitud) => {
+    await processor({
+      usuarioId: 14,
+      email: solicitud.interesado.persona.correoPrimario,
+      asunto: `SIIGES: Atender Observaciones - Folio ${solicitud.folioSolicitud}`,
+      template: 'solicitudRevEquivObservaciones',
+      params: {
+        folioSolicitud: solicitud.folioSolicitud,
+        observaciones: solicitud.observaciones,
+      },
+    });
+  },
+};
+
+async function sendNotificationEstatus(processor, estatus, solicitud) {
+  const action = NOTIFICATION_MAPPING[estatus];
+  if (!action) return;
+  await action(processor, solicitud);
+}
+
 async function updateSolicitudRevEquiv(req, reply) {
   try {
     const data = req.body;
@@ -8,8 +68,14 @@ async function updateSolicitudRevEquiv(req, reply) {
     const updatedEquiv = await this.solicitudRevEquivServices
       .updateSolicitudRevEquiv(data, { id: solicitudRevEquivId });
 
+    // Enviar notificación si hay cambio de estatus
+    if (solicitudRevEquivId) {
+      const processor = this.notificacionServices.sendNotificationEmail;
+      sendNotificationEstatus(processor, updatedEquiv.estatusSolicitudRevEquivId, updatedEquiv);
+    }
+
     return reply
-      .code(200)
+      .code(201)
       .header('Content-Type', 'application/json; charset=utf-8')
       .send({ data: updatedEquiv });
   } catch (error) {
