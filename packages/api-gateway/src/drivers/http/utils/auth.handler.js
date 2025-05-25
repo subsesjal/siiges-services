f8f87518be12cf34151ffa7e90c3dc901436131c
cetherdev <robert.hct@gmail.com>
2025-05-24 19:33:18 -0600

    validate another api key for external services

--- packages/api-gateway/src/drivers/http/utils/auth.handler.js
+++ packages/api-gateway/src/drivers/http/utils/auth.handler.js
@@ -5,13 +5,33 @@ const errorHandler = require('./errorHandler');
 // eslint-disable-next-line consistent-return
 const validateApiKey = (req, reply, next) => {
   try {
-    const apiKey = req.headers.api_key === config.apiKey;
-    const swaggerDoc = req.url.startsWith('/doc');
-    const documents = req.url.startsWith('/uploads');
-    const skipApiKey = swaggerDoc || documents;
-    if (!apiKey !== skipApiKey) {
-      throw boom.unauthorized('Unauthorized access to this API. You need an API KEY');
+    const { url } = req;
+    const apiKeyFromHeader = req.headers.api_key;
+
+    const isSwaggerDoc = url.startsWith('/doc');
+    const isUpload = url.startsWith('/uploads');
+    if (isSwaggerDoc || isUpload) {
+      return next();
     }
+
+    // Missing key
+    if (!apiKeyFromHeader) {
+      throw boom.unauthorized('Acceso no autorizado. Falta la API KEY en el encabezado.');
+    }
+
+    // External route
+    if (url.startsWith('/api/v2/external')) {
+      if (apiKeyFromHeader !== config.externalApiKey) {
+        throw boom.unauthorized('API KEY externa no válida.');
+      }
+      return next();
+    }
+
+    // Public/private
+    if (apiKeyFromHeader !== config.apiKey) {
+      throw boom.unauthorized('API KEY no válida.');
+    }
+
     next();
   } catch (error) {
     return errorHandler(error, reply);
