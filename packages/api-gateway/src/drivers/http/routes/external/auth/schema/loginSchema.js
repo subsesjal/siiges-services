7806829db9a81792c4b8b570006c36860342616b
cetherdev <robert.hct@gmail.com>
2025-05-24 22:31:34 -0600

    add login schema for external users

--- packages/api-gateway/src/drivers/http/routes/external/auth/schema/loginSchema.js
+++ packages/api-gateway/src/drivers/http/routes/external/auth/schema/loginSchema.js
@@ -1,3 +1,5 @@
+const { responseProperties } = require('./properties/responseProperties');
+
 const loginSchema = {
   tags: ['Login'],
   description: 'Login user',
@@ -7,7 +9,34 @@ const loginSchema = {
       usuario: { type: 'string' },
       contrasena: { type: 'string' },
     },
+    required: ['usuario', 'contrasena'],
     examples: [{ usuario: 'arianag', contrasena: 'Aa/1234567890' }],
+  },
+  response: {
+    200: {
+      type: 'object',
+      properties: {
+        data: {
+          type: 'object',
+          properties: {
+            usuario: { type: 'string' },
+            correo: {
+              type: 'string', format: 'email',
+            },
+            ...responseProperties,
+            rol: {
+              type: 'object',
+              properties: {
+                nombre: { type: 'string' },
+                descripcion: { type: 'string' },
+                ...responseProperties,
+              },
+            },
+          },
+        },
+        token: { type: 'string' },
+      },
+    },
   },
 };
 
