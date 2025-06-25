240b7af2f4a97ec1d4fffcaea57f66dc3e856ffb
Arely Rizo <arelyruiz50700@gmail.com>
2025-06-25 09:47:04 -0600

    Endpoint de notificaciones

--- packages/notificaciones/src/useCases/db/notificaciones/find-one.notificacion.use-cases.js
+++ packages/notificaciones/src/useCases/db/notificaciones/find-one.notificacion.use-cases.js
@@ -1,4 +1,12 @@
+const path = require('path');
+const fs = require('fs');
 const { checkers } = require('@siiges-services/shared');
+const templateHelper = require('../../helpers');
+
+const ADD_DETAILS_MAPPING = {
+  observacionSolicitud: templateHelper.generateMapObservaciones,
+  folioDocumentosAlumnos: templateHelper.generateMapFoliosAlumnos,
+};
 
 const findOneNotificaciones = (
   findNotificationQuery,
@@ -6,11 +14,43 @@ const findOneNotificaciones = (
 ) => async ({ id }) => {
   const getNotification = await findNotificationQuery({ id });
   checkers.throwErrorIfDataIsFalsy(getNotification, 'Notificacion', id);
-  let updateNotification;
-  if (getNotification.status !== 'OPENED') {
-    updateNotification = await updateNotificationQuery({ id }, { status: 'OPENED' });
+
+  const response = getNotification;
+  let newStatus = response.status;
+
+  if (newStatus !== 'OPENED') {
+    await updateNotificationQuery({ id }, { status: 'OPENED' });
+    newStatus = 'OPENED';
   }
-  return updateNotification || getNotification;
+
+  const templateName = response.template;
+  let dataParsed = JSON.parse(response.data);
+
+  const detailMapping = ADD_DETAILS_MAPPING[templateName];
+  if (detailMapping) {
+    dataParsed = detailMapping(dataParsed);
+  }
+
+  const templatePath = path.join(__dirname, '..', '..', 'services', 'templates', `${templateName}.html`);
+
+  const templateContent = fs.readFileSync(templatePath, 'utf8');
+
+  let emailHtml = templateContent;
+  Object.entries(dataParsed).forEach(([param, value]) => {
+    let replacedValue = value;
+    if (typeof value === 'string') {
+      replacedValue = value.replace(/\n/g, '<br/>');
+    }
+    const regex = new RegExp(`{${param}}`, 'g');
+    emailHtml = emailHtml.replace(regex, replacedValue);
+  });
+
+  // con  ...response.get({ plain: true }), convertimos la instancia en unobjeto plano
+  return {
+    ...response.get({ plain: true }),
+    status: newStatus,
+    emailHtml,
+  };
 };
 
 module.exports = { findOneNotificaciones };
