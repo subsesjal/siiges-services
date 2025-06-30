db8efa76c114d051da8fe9d30eb1155005ce60e8
elmejorlui <elmejorlui@gmail.com>
2025-06-30 11:02:17 -0600

    fix(tituloElectronico): fixed the schema of titulo electronico

--- packages/api-gateway/src/drivers/http/routes/publics/alumnos/schema/find-one.alumno-titulo.schema.js
+++ packages/api-gateway/src/drivers/http/routes/publics/alumnos/schema/find-one.alumno-titulo.schema.js
@@ -1,5 +1,4 @@
-const { alumno } = require('../../../external/alumnos/schema/properties/alumno');
-const { persona } = require('../../../external/alumnos/schema/properties/persona');
+const { tituloElectronico } = require('./properties/tituloElectronico');
 const { responseProperties } = require('../../../external/alumnos/schema/properties/responseProperties');
 
 const findAlumnoTitulo = {
@@ -10,74 +9,13 @@ const findAlumnoTitulo = {
     properties: { folioControl: { type: 'string' } },
     required: ['folioControl'],
   },
-  body: {
-    type: 'object',
-    properties: {
-      folioControl: { type: 'string' },
-      alumnos: {
-        type: 'array',
-        items: {
-          type: 'object',
-          properties: {
-            ...alumno,
-            persona: {
-              type: 'object',
-              properties: {
-                ...persona,
-              },
-              required: [
-                'nombre',
-                'apellidoPaterno',
-                'fechaNacimiento',
-                'sexo',
-                'nacionalidad',
-                'curp',
-                'celular',
-                'correoPrimario',
-              ],
-            },
-          },
-          required: ['matricula', 'persona'],
-        },
-      },
-    },
-    required: ['folioControl', 'alumnos'],
-  },
   response: {
-    201: {
+    200: {
       type: 'object',
       properties: {
-        data: {
-          type: 'object',
-          properties: {
-            successes: { type: 'integer' },
-            failures: { type: 'integer' },
-            alumnos: {
-              type: 'array',
-              items: {
-                type: 'object',
-                properties: {
-                  success: { type: 'boolean' },
-                  error: { nullable: true },
-                  alumno: {
-                    type: 'object',
-                    properties: {
-                      ...alumno,
-                      ...responseProperties,
-                      persona: {
-                        type: 'object',
-                        properties: {
-                          ...persona,
-                          ...responseProperties,
-                        },
-                      },
-                    },
-                  },
-                },
-              },
-            },
-          },
-        },
+        id: { type: 'integer' },
+        ...tituloElectronico,
+        ...responseProperties,
       },
     },
   },
