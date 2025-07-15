2359dbae8b4faf6baae9920b60b0995a4fe5b12d
cetherdev <robert.hct@gmail.com>
2025-07-14 21:49:15 -0600

    fix path

--- packages/files/src/adapters/fs/directories/path.directories.fs.adpaters.js
+++ packages/files/src/adapters/fs/directories/path.directories.fs.adpaters.js
@@ -2,7 +2,7 @@ const path = require('path');
 const { config } = require('../../../../config/environment');
 
 function createPath(tipoEntidad, tipoDocumento) {
-  return path.join(__dirname, config.pathFile, 'public', 'uploads', tipoEntidad, tipoDocumento);
+  return path.join(config.pathFile, 'public', 'uploads', tipoEntidad, tipoDocumento);
 }
 
 module.exports = { createPath };
