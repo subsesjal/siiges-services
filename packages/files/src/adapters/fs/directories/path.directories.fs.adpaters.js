9c32defebfe0bd65b7e640de5e596d3885479317
Daniel Calamateo <96149805+Calamateo@users.noreply.github.com>
2025-07-23 20:33:58 -0600

    Update path.directories.fs.adpaters.js

--- packages/files/src/adapters/fs/directories/path.directories.fs.adpaters.js
+++ packages/files/src/adapters/fs/directories/path.directories.fs.adpaters.js
@@ -2,7 +2,7 @@ const path = require('path');
 const { config } = require('../../../../config/environment');
 
 function createPath(tipoEntidad, tipoDocumento) {
-  return path.join(config.pathFile, 'public', 'uploads', tipoEntidad, tipoDocumento);
+  return path.join(process.env.PATH_FILE, 'public', 'uploads', tipoEntidad, tipoDocumento);
 }
 
 module.exports = { createPath };
