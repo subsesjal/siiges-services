26f72ae681f008ee8a448e95aed0987e49168bfe
Daniel Calamateo <96149805+Calamateo@users.noreply.github.com>
2025-07-23 21:44:52 -0600

    Update upload-dir-path.js

--- packages/files/src/utils/constants/upload-dir-path.js
+++ packages/files/src/utils/constants/upload-dir-path.js
@@ -1,6 +1,6 @@
 const { join } = require('path');
 const { constants } = require('@siiges-services/shared');
 
-const uploadDir = join(process.env.PATH_FILEFILE, 'public');
+const uploadDir = join(process.env.PATH_FILE, 'public');
 
 module.exports = uploadDir;
