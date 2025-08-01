ed8a08ab99971ab28c58beecde790a4a551c3c93
cetherdev <robert.hct@gmail.com>
2025-07-31 20:25:17 -0600

    SDT-1115: Fix PDF TITULO

--- packages/files/src/useCases/db/TITULO_ELECTRONICO/index.js
+++ packages/files/src/useCases/db/TITULO_ELECTRONICO/index.js
@@ -10,7 +10,7 @@ module.exports = {
     xmlAdapter.createTituloElectronicoQuery,
   ),
   findFileTitulo: findFileTitulo(
-    xmlAdapter.findOneAlumnoTituloElectronicoQuery,
+    xmlAdapter.findOneTituloElectronicoQuery,
     xmlAdapter.findOneFileQuery,
     GenerarTitulo,
   ),
