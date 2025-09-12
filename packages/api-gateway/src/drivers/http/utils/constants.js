c1eb1e577b2ec32ef2032ff64b6fa2ceae376dbb
Calamateo <dany.calamateo@gmail.com>
2025-09-12 12:42:31 -0600

    fix: actualizar tipos de archivo y aumentar tamaño máximo de archivo

--- packages/api-gateway/src/drivers/http/utils/constants.js
+++ packages/api-gateway/src/drivers/http/utils/constants.js
@@ -19,8 +19,24 @@ const tipoExtension = [
     mimeType: 'text/xml',
     ext: '.xml',
   },
+  {
+    mimeType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    ext: '.xlsx',
+  },
+  {
+    mimeType: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+    ext: '.docx',
+  },
+  {
+    mimeType: 'application/vnd.ms-excel',
+    ext: '.xls',
+  },
+  {
+    mimeType: 'application/msword',
+    ext: '.doc',
+  },
 ];
 
-const maxFileSize = 3 * 1024 * 1024;
+const maxFileSize = 5 * 1024 * 1024;
 
 module.exports = { tipoExtension, maxFileSize };
