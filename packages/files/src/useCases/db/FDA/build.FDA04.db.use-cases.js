8e884ec74f35cc18b7fad201349b2b8e84297197
Zenitramhgx <zenitramhgx15@hotmail.com>
2025-10-14 16:22:37 -0600

    SDT-1240: fix: Descarga FDA04 Y Aulas plantel correctas FDA04

--- packages/files/src/useCases/db/FDA/build.FDA04.db.use-cases.js
+++ packages/files/src/useCases/db/FDA/build.FDA04.db.use-cases.js
@@ -1,16 +1,19 @@
+const { Op } = require('sequelize');
 const { checkers } = require('@siiges-services/shared');
 
 const buildFileFDA04 = (
   findOneSolicitudProgramaQuery,
+  findAllInfraestructura,
+  findAllInfraestructuraPrograma,
+  findOneInfraestructura,
+  findPlantelHigieneByPlantelId,
+  findSaludInstitucionesByPlantelId,
   createPhpFile,
 ) => async (solicitudId, tipoDocumento) => {
+  const AULA_ID = 1;
   const include = [{
     association: 'programa',
-    attributes: [
-      'id',
-      'duracionPeriodos',
-      'nombre',
-    ],
+    attributes: ['id', 'duracionPeriodos', 'nombre'],
     include: [
       { association: 'ciclo', attributes: ['id'] },
       { association: 'nivel', attributes: ['id', 'descripcion'] },
@@ -37,19 +40,6 @@ const buildFileFDA04 = (
             include: [{ association: 'edificioNivel' }],
           },
           { association: 'plantelSeguridadSistemas' },
-          { association: 'plantelHigienes' },
-          { association: 'saludInstituciones' },
-          {
-            association: 'infraestructuras',
-            include: [{
-              association: 'asignaturasInfraestructura',
-              include: [
-                {
-                  association: 'asignatura',
-                  attributes: ['id', 'clave'],
-                }],
-            }],
-          },
           {
             association: 'domicilio',
             include: [
@@ -59,7 +49,8 @@ const buildFileFDA04 = (
           },
           { association: 'institucion', attributes: ['id', 'nombre'] },
         ],
-      }],
+      },
+    ],
   },
   {
     association: 'usuario',
@@ -67,15 +58,63 @@ const buildFileFDA04 = (
   },
   { association: 'estatusSolicitud' }];
 
-  const solicitud = await findOneSolicitudProgramaQuery({ id: solicitudId }, {
-    undefined,
-    include,
-    strict: false,
-  });
+  const solicitud = await findOneSolicitudProgramaQuery(
+    { id: solicitudId },
+    { include, strict: false },
+  );
 
   checkers.throwErrorIfDataIsFalsy(solicitud, 'solicitud', solicitudId);
 
-  const file = await createPhpFile(solicitud.toJSON(), tipoDocumento);
+  const solicitudData = solicitud.toJSON();
+
+  const includeInfraestructura = [
+    { association: 'tipoInstalacion' },
+    {
+      association: 'asignaturasInfraestructura',
+      include: [{ association: 'asignatura' }],
+    },
+  ];
+
+  const infraestructurasGenerales = await findAllInfraestructura(
+    {
+      plantelId: solicitudData?.programa?.plantel?.id,
+      tipoInstalacionId: { [Op.not]: AULA_ID },
+    },
+    { include: includeInfraestructura, strict: false },
+  );
+
+  const aulasAsociadas = await findAllInfraestructuraPrograma(
+    { programaId: solicitudData?.programa?.id },
+  );
+
+  const aulasDetalladas = await Promise.all(
+    (aulasAsociadas || []).map(async (item) => {
+      const aula = await findOneInfraestructura(
+        { id: item.infraestructuraId },
+        { include: includeInfraestructura, strict: false },
+      );
+      return aula?.toJSON?.() ?? null;
+    }),
+  );
+
+  const todasInfraestructuras = [
+    ...(infraestructurasGenerales || []),
+    ...aulasDetalladas.filter(Boolean),
+  ];
+
+  const plantelHigienes = await findPlantelHigieneByPlantelId({
+    plantelId: solicitudData?.programa?.plantel?.id,
+  });
+
+  const saludInstituciones = await findSaludInstitucionesByPlantelId({
+    plantelId: solicitudData?.programa?.plantel?.id,
+  });
+
+  solicitudData.programa.plantel.infraestructuras = todasInfraestructuras;
+  solicitudData.programa.plantel.plantelHigienes = plantelHigienes;
+  solicitudData.programa.plantel.saludInstituciones = saludInstituciones;
+
+  const file = await createPhpFile(solicitudData, tipoDocumento);
   return Buffer.from(file);
 };
 
