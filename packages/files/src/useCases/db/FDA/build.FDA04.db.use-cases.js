59c999ce9fa86be7e2e5aa92ca462c97e0c62f1f
Zenitramhgx <zenitramhgx15@hotmail.com>
2025-10-14 16:22:37 -0600

    SDT-1240: fix: Descarga FDA04 Y Aulas plantel correctas FDA04

--- packages/files/src/useCases/db/FDA/build.FDA04.db.use-cases.js
+++ packages/files/src/useCases/db/FDA/build.FDA04.db.use-cases.js
@@ -1,9 +1,16 @@
+const { Op } = require('sequelize');
 const { checkers } = require('@siiges-services/shared');
 
 const buildFileFDA04 = (
   findOneSolicitudProgramaQuery,
+  findAllInfraestructura,
+  findAllInfraestructuraPrograma,
+  findOneInfraestructura,
+  findPlantelHigieneByPlantelId,
+  findSaludInstitucionesByPlantelId,
   createPhpFile,
 ) => async (solicitudId, tipoDocumento) => {
+  const AULA_ID = 1;
   const include = [{
     association: 'programa',
     include: [
@@ -36,7 +43,8 @@ const buildFileFDA04 = (
           },
           { association: 'institucion' },
         ],
-      }],
+      },
+    ],
   },
   {
     association: 'usuario',
@@ -44,15 +52,63 @@ const buildFileFDA04 = (
   },
   { association: 'estatusSolicitud' }];
 
-  const solicitud = await findOneSolicitudProgramaQuery({ id: solicitudId }, {
-    undefined,
-    include,
-    strict: false,
-  });
+  const solicitud = await findOneSolicitudProgramaQuery(
+    { id: solicitudId },
+    { include, strict: false },
+  );
 
   checkers.throwErrorIfDataIsFalsy(solicitud, 'solicitud', solicitudId);
 
-  const file = await createPhpFile(solicitud.toJSON(), tipoDocumento);
+  const solicitudData = solicitud.toJSON();
+
+  const includeInfraestructura = [
+    { association: 'tipoInstalacion' },
+    {
+      association: 'asignaturasInfraestructura',
+      include: [{ association: 'asignatura' }],
+    },
+  ];
+
+  const infraestructurasGenerales = await findAllInfraestructura(
+    {
+      plantelId: solicitudData?.programa?.plantel?.id,
+      tipoInstalacionId: { [Op.not]: AULA_ID },
+    },
+    { include: includeInfraestructura, strict: false },
+  );
+
+  const aulasAsociadas = await findAllInfraestructuraPrograma(
+    { programaId: solicitudData?.programa?.id },
+  );
+
+  const aulasDetalladas = await Promise.all(
+    (aulasAsociadas || []).map(async (item) => {
+      const aula = await findOneInfraestructura(
+        { id: item.infraestructuraId },
+        { include: includeInfraestructura, strict: false },
+      );
+      return aula?.toJSON?.() ?? null;
+    }),
+  );
+
+  const todasInfraestructuras = [
+    ...(infraestructurasGenerales || []),
+    ...aulasDetalladas.filter(Boolean),
+  ];
+
+  const plantelHigienes = await findPlantelHigieneByPlantelId({
+    plantelId: solicitudData?.programa?.plantel?.id,
+  });
+
+  const saludInstituciones = await findSaludInstitucionesByPlantelId({
+    plantelId: solicitudData?.programa?.plantel?.id,
+  });
+
+  solicitudData.programa.plantel.infraestructuras = todasInfraestructuras;
+  solicitudData.programa.plantel.plantelHigienes = plantelHigienes;
+  solicitudData.programa.plantel.saludInstituciones = saludInstituciones;
+
+  const file = await createPhpFile(solicitudData, tipoDocumento);
   return Buffer.from(file);
 };
 
