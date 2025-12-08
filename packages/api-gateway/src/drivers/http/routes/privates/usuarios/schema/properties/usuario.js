6e7917bf4bb634b79def9390ab3b0cbc63840dec
Daniel Calamateo <96149805+Calamateo@users.noreply.github.com>
2025-12-08 12:27:29 -0600

    Update 'correo' field maxLength to 80
    Increased the maximum length for the 'correo' field from 50 to 80 characters.

--- packages/api-gateway/src/drivers/http/routes/privates/usuarios/schema/properties/usuario.js
+++ packages/api-gateway/src/drivers/http/routes/privates/usuarios/schema/properties/usuario.js
@@ -3,7 +3,7 @@ const usuario = {
   rolId: { type: 'integer' },
   usuario: { type: 'string', minLength: 3, maxLength: 25 },
   correo: {
-    type: 'string', format: 'email', minLength: 3, maxLength: 50,
+    type: 'string', format: 'email', minLength: 3, maxLength: 80,
   },
   estatus: { type: 'integer', minimum: 0, maximum: 1 },
   actualizado: { type: 'boolean' },
