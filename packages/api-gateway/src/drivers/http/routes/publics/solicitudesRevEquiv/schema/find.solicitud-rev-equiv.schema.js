55fb8d421ab497193b105c67a9ec94a2236caed7
JoeRootX <elsuperjoy@gmail.com>
2025-12-09 12:57:39 -0600

    SDT-1401: se solvento observacion

--- packages/api-gateway/src/drivers/http/routes/publics/solicitudesRevEquiv/schema/find.solicitud-rev-equiv.schema.js
+++ packages/api-gateway/src/drivers/http/routes/publics/solicitudesRevEquiv/schema/find.solicitud-rev-equiv.schema.js
@@ -1,21 +1,80 @@
-const findSolicitudRevEquivSchema = {
+const { domicilio } = require('../../../privates/solicitudes/representantes/schemas/properties/domicilio.properties');
+const { persona } = require('../../../privates/usuarios/schema/properties/persona');
+const { solicitudRevEquiv } = require('../../../privates/solicitudesRevEquiv/schema/properties/solicitudRevEquiv');
+const { responseProperties } = require('../../../privates/solicitudesRevEquiv/schema/properties/responseProperties');
+const { institucionProcedencia } = require('../../../privates/solicitudesRevEquiv/schema/properties/institucionProcedencia');
+const { institucionDestino } = require('../../../privates/solicitudesRevEquiv/schema/properties/institucionDestino');
+const { asignaturaAntecedenteEquivalente } = require('../../../privates/solicitudesRevEquiv/schema/properties/asignaturaAntecedenteEquivalente');
+const { institucionDestinoPrograma } = require('../../../privates/solicitudesRevEquiv/schema/properties/institucionDestinoPrograma');
+
+const findEquivalenciaSchema = {
   type: 'object',
-  description: 'Find solicitudRevEquiv by folio (query param).',
-  querystring: {
-    type: 'object',
-    properties: {
-      folioSolicitud: { type: 'string' },
-    },
-    required: ['folioSolicitud'],
-  },
+  description: 'Create a new equivalencia.',
   response: {
     200: {
       type: 'object',
       properties: {
-        data: { type: ['object', 'null'] },
+        data: {
+          type: 'object',
+          properties: {
+            id: { type: 'integer' },
+            ...solicitudRevEquiv,
+            ...responseProperties,
+            interesado: {
+              type: 'object',
+              properties: {
+                persona: {
+                  type: 'object',
+                  properties: {
+                    ...persona,
+                    ...responseProperties,
+                    domicilio: {
+                      type: 'object',
+                      properties: {
+                        ...domicilio,
+                        ...responseProperties,
+                      },
+                    },
+                  },
+                },
+                institucionProcedencia: {
+                  type: 'object',
+                  properties: {
+                    ...institucionProcedencia,
+                    ...responseProperties,
+                  },
+                },
+                institucionDestino: {
+                  type: 'object',
+                  properties: {
+                    ...institucionDestino,
+                    ...responseProperties,
+                    institucionDestinoPrograma: {
+                      type: ['object', 'null'],
+                      properties: {
+                        ...institucionDestinoPrograma,
+                        ...responseProperties,
+                      },
+                    },
+                  },
+                },
+                asignaturasAntecedenteEquivalente: {
+                  type: 'array',
+                  items: {
+                    type: 'object',
+                    properties: {
+                      ...asignaturaAntecedenteEquivalente,
+                      ...responseProperties,
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
       },
     },
   },
 };
 
-module.exports = { findSolicitudRevEquivSchema };
+module.exports = findEquivalenciaSchema;
