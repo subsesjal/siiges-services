a1493da206f9813b89c95dedd133289c8204fd65
JoeRootX <elsuperjoy@gmail.com>
2025-12-10 12:56:26 -0600

    SDT-1408:  se modifico el update

--- packages/solicitudesRevEquiv/src/use-cases/db/solicitudesRevEquiv/update.solicitud-rev-equiv.use-cases.js
+++ packages/solicitudesRevEquiv/src/use-cases/db/solicitudesRevEquiv/update.solicitud-rev-equiv.use-cases.js
@@ -2,7 +2,6 @@ const { checkers } = require('@siiges-services/shared');
 
 const updateSolicitudRevEquiv = (
   findOneSolicitudRevEquivQuery,
-  updateSolicitudRevEquivQuery,
 ) => async (data, identifierObj) => {
   const include = [
     {
@@ -15,14 +14,55 @@ const updateSolicitudRevEquiv = (
     },
   ];
 
-  const solicitudRevEquiv = await findOneSolicitudRevEquivQuery(identifierObj);
-  checkers.throwErrorIfDataIsFalsy(solicitudRevEquiv, 'solicitudes-rev-equiv', identifierObj.id);
+  const solicitud = await findOneSolicitudRevEquivQuery(identifierObj, { include });
 
-  return updateSolicitudRevEquivQuery(
-    identifierObj,
-    data,
-    { include },
+  checkers.throwErrorIfDataIsFalsy(
+    solicitud,
+    'solicitudes-rev-equiv',
+    identifierObj.id,
   );
+
+  const { interesado } = solicitud;
+  const persona = interesado?.persona;
+  const domicilio = persona?.domicilio;
+  const institProc = interesado?.institucionProcedencia;
+  const institDest = interesado?.institucionDestino;
+
+  if (data.interesado?.persona?.domicilio) {
+    await domicilio.update(data.interesado.persona.domicilio);
+  }
+
+  if (data.interesado?.persona) {
+    const personaData = { ...data.interesado.persona };
+    delete personaData.domicilio;
+    await persona.update(personaData);
+  }
+
+  if (data.interesado) {
+    const interesadoData = { ...data.interesado };
+    delete interesadoData.persona;
+    delete interesadoData.institucionDestino;
+    delete interesadoData.institucionProcedencia;
+
+    await interesado.update(interesadoData);
+  }
+
+  if (data.interesado?.institucionProcedencia) {
+    await institProc.update(data.interesado.institucionProcedencia);
+  }
+
+  if (data.interesado?.institucionDestino) {
+    const destData = { ...data.interesado.institucionDestino };
+    delete destData.programa;
+    await institDest.update(destData);
+  }
+
+  const solicitudData = { ...data };
+  delete solicitudData.interesado;
+
+  await solicitud.update(solicitudData);
+
+  return findOneSolicitudRevEquivQuery(identifierObj, { include });
 };
 
 module.exports = updateSolicitudRevEquiv;
