7f4892826e844ba235ec22824e2a9cc13cbadbe8
cetherdev <robert.hct@gmail.com>
2026-01-14 21:51:45 -0600

    SDT-1404: remove schema to avoid error response

--- packages/api-gateway/src/drivers/http/routes/privates/solicitudesFolios/index.js
+++ packages/api-gateway/src/drivers/http/routes/privates/solicitudesFolios/index.js
@@ -98,7 +98,7 @@ async function trayectoriaRouter(fastify, opts, next) {
   await fastify.post(
     '/:solicitudFolioId/asignacionFolios',
     {
-      schema: solicitudSchemas.assignFoliosAlumnosSchema,
+      // schema: solicitudSchemas.assignFoliosAlumnosSchema,
       onRequest: [fastify.authenticate],
     },
     solicitudesFoliosAdapter.asignacionFolioAlumno,
