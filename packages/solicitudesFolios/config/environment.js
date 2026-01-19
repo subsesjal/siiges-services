5489224ecc90062ff2ece3d56b2002b6c036f95b
Zenitramhgx <zenitramhgx15@hotmail.com>
2026-01-19 15:17:17 -0600

    SDT-1432: feat: Add variables

--- packages/solicitudesFolios/config/environment.js
+++ packages/solicitudesFolios/config/environment.js
@@ -2,6 +2,14 @@ const { dotenv } = require('@siiges-services/shared');
 
 const config = {
   apiBaseUrl: dotenv.getEnvironmentVar('API_BASE_URL'),
+  // Firma Electrónica - Servicio Jalisco
+  firmaElectronica: {
+    baseUrl: dotenv.getEnvironmentVar('FIRMA_ELECTRONICA_BASE_URL'),
+    clientId: dotenv.getEnvironmentVar('FIRMA_ELECTRONICA_CLIENT_ID'),
+    clientSecret: dotenv.getEnvironmentVar('FIRMA_ELECTRONICA_CLIENT_SECRET'),
+    username: dotenv.getEnvironmentVar('FIRMA_ELECTRONICA_USERNAME'),
+    password: dotenv.getEnvironmentVar('FIRMA_ELECTRONICA_PASSWORD'),
+  },
 };
 
 module.exports = {
