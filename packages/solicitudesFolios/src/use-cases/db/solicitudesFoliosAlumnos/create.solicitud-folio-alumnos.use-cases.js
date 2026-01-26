9658ead2688e064098cc3faba3311587f14df02f
Arely Rizo <arelyruiz50700@gmail.com>
2026-01-26 12:00:40 -0600

    SDT-1458: Ajustes visuales modulo de folios

--- packages/solicitudesFolios/src/use-cases/db/solicitudesFoliosAlumnos/create.solicitud-folio-alumnos.use-cases.js
+++ packages/solicitudesFolios/src/use-cases/db/solicitudesFoliosAlumnos/create.solicitud-folio-alumnos.use-cases.js
@@ -11,6 +11,7 @@ const createSolicitudFolioAlumno = (
   findOneSolicitudFolioQuery,
   createAlumnoFolioQuery,
   findOneSolicitudFolioAlumnoQuery,
+  countSolicitudFolioAlumnosQuery,
 ) => async (data) => {
   const solicitudFolio = await findOneSolicitudFolioQuery({ id: data.solicitudFolioId });
   checkers.throwErrorIfDataIsFalsy(solicitudFolio, 'solicitudes-folios', data.solicitudFolioId);
@@ -34,8 +35,13 @@ const createSolicitudFolioAlumno = (
     throw boom.conflict('Alumno validation status is not valid');
   }
 
-  const result = await createAlumnoFolioQuery(data);
+  const totalAlumnos = await countSolicitudFolioAlumnosQuery({
+    solicitudFolioId: data.solicitudFolioId,
+  }, { isDeleting: false });
 
+  const consecutivo = totalAlumnos + 1;
+
+  const result = await createAlumnoFolioQuery({ ...data, consecutivo });
   const include = [
     {
       association: 'alumno',
