d50168c244699181084c64f9e0be954e92851595
Arely Rizo <arelyruiz50700@gmail.com>
2026-01-29 13:53:21 -0600

    SDT-1445: Se renombro apiBaseUrl

--- packages/solicitudesFolios/config/environment.js
+++ packages/solicitudesFolios/config/environment.js
@@ -1,7 +1,7 @@
 const { dotenv } = require('@siiges-services/shared');
 
 const config = {
-  apiBaseUrl: dotenv.getEnvironmentVar('API_BASE_URL'),
+  urlTitulacion: dotenv.getEnvironmentVar('URL_TITULACION'),
 };
 
 module.exports = {
