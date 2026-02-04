816a74dc2f7984914dae6907e2b0e1f7c8fb49d9
Arely Rizo <arelyruiz50700@gmail.com>
2026-02-04 15:14:05 -0600

    SDT-1481: Ajustes del número consecutivo, al eliminar alumno

--- packages/solicitudesFolios/src/use-cases/db/solicitudesFoliosAlumnos/delete.solicitud-folio-alumno.use-cases.js
+++ packages/solicitudesFolios/src/use-cases/db/solicitudesFoliosAlumnos/delete.solicitud-folio-alumno.use-cases.js
@@ -3,13 +3,38 @@ const { checkers } = require('@siiges-services/shared');
 const deleteSolicitudFolioAlumno = (
   findOneSolicitudFolioAlumnoQuery,
   deleteSolicitudFolioAlumnoQuery,
+  updateSolicitudFolioAlumnoQuery,
+  findAllSolicitudFolioAlumnosQuery,
 ) => async ({ id }) => {
   const alumno = await findOneSolicitudFolioAlumnoQuery({ id });
   checkers.throwErrorIfDataIsFalsy(alumno, 'solicitudes-folios-alumnos', id);
 
-  const alumnoDeleted = await deleteSolicitudFolioAlumnoQuery({ id });
+  const { solicitudFolioId } = alumno;
 
-  return alumnoDeleted;
+  await deleteSolicitudFolioAlumnoQuery({ id });
+
+  const alumnos = await findAllSolicitudFolioAlumnosQuery(
+    { solicitudFolioId },
+    { order: [['consecutivo', 'ASC']] },
+  );
+
+  let consecutivo = 1;
+
+  for (let i = 0; i < alumnos.length; i += 1) {
+    const item = alumnos[i];
+
+    if (item.consecutivo !== consecutivo) {
+      // eslint-disable-next-line no-await-in-loop
+      await updateSolicitudFolioAlumnoQuery(
+        { id: item.id },
+        { consecutivo },
+      );
+    }
+
+    consecutivo += 1;
+  }
+
+  return true;
 };
 
 module.exports = deleteSolicitudFolioAlumno;
