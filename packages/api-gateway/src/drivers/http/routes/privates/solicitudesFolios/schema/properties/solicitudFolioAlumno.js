febbf0dac2c0bdb52062c80e4a1bbb846216abad
Arely Rizo <arelyruiz50700@gmail.com>
2026-02-06 10:17:41 -0600

    SDT-1480: Cambios para la fecha de registro y fecha expedicion

--- packages/api-gateway/src/drivers/http/routes/privates/solicitudesFolios/schema/properties/solicitudFolioAlumno.js
+++ packages/api-gateway/src/drivers/http/routes/privates/solicitudesFolios/schema/properties/solicitudFolioAlumno.js
@@ -8,7 +8,7 @@ const solicitudFolioAlumno = {
   folioActa: { type: 'string' },
   fechaInicio: { type: 'string', format: 'date-time' },
   fechaTerminacion: { type: 'string', format: 'date-time' },
-  fechaExpedicion: { type: 'string', format: 'date-time' },
+  // fechaExpedicion: { type: 'string', format: 'date-time' },
   fechaExamenProfesional: { type: 'string', format: 'date-time' },
 };
 
