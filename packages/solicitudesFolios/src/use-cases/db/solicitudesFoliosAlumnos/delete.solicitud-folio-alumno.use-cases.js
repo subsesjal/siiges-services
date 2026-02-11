587f4a3218773ef6a9e00b5455da080a6bb93ccf
Arely Rizo <arelyruiz50700@gmail.com>
2026-02-11 09:50:46 -0600

    SDT-1481: manejar consecutivos

--- packages/solicitudesFolios/src/use-cases/db/solicitudesFoliosAlumnos/delete.solicitud-folio-alumno.use-cases.js
+++ packages/solicitudesFolios/src/use-cases/db/solicitudesFoliosAlumnos/delete.solicitud-folio-alumno.use-cases.js
@@ -18,21 +18,12 @@ const deleteSolicitudFolioAlumno = (
     { order: [['consecutivo', 'ASC']] },
   );
 
-  let consecutivo = 1;
-
-  for (let i = 0; i < alumnos.length; i += 1) {
-    const item = alumnos[i];
-
-    if (item.consecutivo !== consecutivo) {
-      // eslint-disable-next-line no-await-in-loop
-      await updateSolicitudFolioAlumnoQuery(
-        { id: item.id },
-        { consecutivo },
-      );
-    }
-
-    consecutivo += 1;
-  }
+  await Promise.all(
+    alumnos.map((item, index) => updateSolicitudFolioAlumnoQuery(
+      { id: item.id },
+      { consecutivo: index + 1 },
+    )),
+  );
 
   return true;
 };
