0c63ec0b88c6855b192664376964d11f35410e9a
Zenitramhgx <zenitramhgx15@hotmail.com>
2026-09-09 12:37:14 -0600

    SDT-1735: feat: General & Specific Catalogo Titulos Electronicos

--- packages/api-gateway/src/drivers/http/adapters/administracionAcademica/titulosElectronicos/handlers/find-all.handlers.titulos.adapters.js
+++ packages/api-gateway/src/drivers/http/adapters/administracionAcademica/titulosElectronicos/handlers/find-all.handlers.titulos.adapters.js
@@ -4,9 +4,23 @@ const errorHandler = require('../../../../utils/errorHandler');
 async function findAllTitulos(req, reply) {
   try {
     Logger.info('[TitulosElectronicos]: findTitulos - Request received');
-    const { numeroRvoe } = req.query;
+    const {
+      institucionId,
+      numeroRvoe,
+      nombre,
+      primerApellido,
+      segundoApellido,
+      curp,
+    } = req.query;
 
-    const titulos = await this.administracionAcademicaServices.findAllTitulos({ numeroRvoe });
+    const titulos = await this.administracionAcademicaServices.findAllTitulos({
+      institucionId,
+      numeroRvoe,
+      nombre,
+      primerApellido,
+      segundoApellido,
+      curp,
+    });
 
     return reply
       .code(200)
