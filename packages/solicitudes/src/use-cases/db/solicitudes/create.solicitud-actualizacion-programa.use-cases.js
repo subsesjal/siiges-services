fb784537616d3aa5b471cb3d9c7e577ab7f4962d
JoeRootX <joeroot.dev@gmail.com>
2026-09-23 17:16:46 -0600

    SDT-1746(refactor): unificar clonación de solicitudes de actualización y cambio en factory parametrizado

--- packages/solicitudes/src/use-cases/db/solicitudes/create.solicitud-actualizacion-programa.use-cases.js
+++ packages/solicitudes/src/use-cases/db/solicitudes/create.solicitud-actualizacion-programa.use-cases.js
@@ -1,67 +1,9 @@
-const { checkers } = require('@siiges-services/shared');
-const boom = require('@hapi/boom');
-const removeIds = require('../../../utils/remove-ids.utils');
-const { createFolioSolicitud } = require('../../../utils/create-folio.utils');
+const buildCloneSolicitudProgramaUseCase = require('./create.solicitud-clon-programa.use-cases');
 
-const createActualizacionSolicitudPrograma = (
-  findOneSolicitudQuery,
-  countSolicitudesQuery,
-  createSolicitudProgramaQuery,
-  findOneEstatusSolicitudQuery,
-) => async (identifierObj, data) => {
-  const { tipoSolicitudId } = data;
-  const { solicitudId } = identifierObj;
-
-  if (tipoSolicitudId !== 5) {
-    throw boom.badRequest(
-      '[Solicitudes]: El tipo de solicitud no es una actualización',
-    );
-  }
-
-  const estatusSolicitud = await findOneEstatusSolicitudQuery({ id: 1 });
-  checkers.throwErrorIfDataIsFalsy(estatusSolicitud, 'estatus_solicitudes', 1);
-
-  const include = [{
-    association: 'programa',
-    include: [
-      { association: 'programaTurnos' },
-      { association: 'asignaturas' },
-      { association: 'trayectoria' },
-    ],
-  }];
-
-  const solicitud = await findOneSolicitudQuery({ id: solicitudId }, {
-    undefined,
-    include,
-    strict: false,
-  });
-  checkers.throwErrorIfDataIsFalsy(solicitud, 'solicitudes', solicitudId);
-
-  const solicitudData = solicitud.toJSON();
-
-  const solicitudDataWithoutIds = removeIds(solicitudData, {
-    keep: ['acuerdoRvoe', 'fechaSurteEfecto'],
-  });
-
-  const totalSolicitudes = await countSolicitudesQuery();
-  const folioSolcitud = createFolioSolicitud(totalSolicitudes, solicitudDataWithoutIds
-    .programa.nivelId);
-
-  const newData = {
-    ...solicitudDataWithoutIds,
-    folio: folioSolcitud,
-    tipoSolicitudId: 5,
-    estatusSolicitudId: 1,
-    usuarioId: data.usuarioId,
-  };
-
-  const newSolicitud = await createSolicitudProgramaQuery({
-    ...newData,
-  }, include);
-
-  checkers.throwErrorIfDataIsFalsy(newSolicitud, 'solicitudes', newSolicitud.id);
-
-  return newSolicitud;
-};
+const createActualizacionSolicitudPrograma = buildCloneSolicitudProgramaUseCase({
+  tipoSolicitudId: 5,
+  mensajeError: '[Solicitudes]: El tipo de solicitud no es una actualización',
+  keep: ['acuerdoRvoe', 'fechaSurteEfecto'],
+});
 
 module.exports = createActualizacionSolicitudPrograma;
--- packages/solicitudes/src/use-cases/db/solicitudes/create.solicitud-cambio-nombre-programa.use-cases.js
+++ packages/solicitudes/src/use-cases/db/solicitudes/create.solicitud-cambio-nombre-programa.use-cases.js
@@ -1,65 +1,8 @@
-const { checkers } = require('@siiges-services/shared');
-const boom = require('@hapi/boom');
-const removeIds = require('../../../utils/remove-ids.utils');
-const { createFolioSolicitud } = require('../../../utils/create-folio.utils');
+const buildCloneSolicitudProgramaUseCase = require('./create.solicitud-clon-programa.use-cases');
 
-const createCambioNombreSolicitudPrograma = (
-  findOneSolicitudQuery,
-  countSolicitudesQuery,
-  createSolicitudProgramaQuery,
-  findOneEstatusSolicitudQuery,
-) => async (identifierObj, data) => {
-  const { tipoSolicitudId } = data;
-  const { solicitudId } = identifierObj;
-
-  if (tipoSolicitudId !== 6) {
-    throw boom.badRequest(
-      '[Solicitudes]: El tipo de solicitud no es un cambio de nombre de institución',
-    );
-  }
-
-  const estatusSolicitud = await findOneEstatusSolicitudQuery({ id: 1 });
-  checkers.throwErrorIfDataIsFalsy(estatusSolicitud, 'estatus_solicitudes', 1);
-
-  const include = [{
-    association: 'programa',
-    include: [
-      { association: 'programaTurnos' },
-      { association: 'asignaturas' },
-      { association: 'trayectoria' },
-    ],
-  }];
-
-  const solicitud = await findOneSolicitudQuery({ id: solicitudId }, {
-    undefined,
-    include,
-    strict: false,
-  });
-  checkers.throwErrorIfDataIsFalsy(solicitud, 'solicitudes', solicitudId);
-
-  const solicitudData = solicitud.toJSON();
-
-  const solicitudDataWithoutIds = removeIds(solicitudData);
-
-  const totalSolicitudes = await countSolicitudesQuery();
-  const folioSolcitud = createFolioSolicitud(totalSolicitudes, solicitudDataWithoutIds
-    .programa.nivelId);
-
-  const newData = {
-    ...solicitudDataWithoutIds,
-    folio: folioSolcitud,
-    tipoSolicitudId: 6,
-    estatusSolicitudId: 1,
-    usuarioId: data.usuarioId,
-  };
-
-  const newSolicitud = await createSolicitudProgramaQuery({
-    ...newData,
-  }, include);
-
-  checkers.throwErrorIfDataIsFalsy(newSolicitud, 'solicitudes', newSolicitud.id);
-
-  return newSolicitud;
-};
+const createCambioNombreSolicitudPrograma = buildCloneSolicitudProgramaUseCase({
+  tipoSolicitudId: 6,
+  mensajeError: '[Solicitudes]: El tipo de solicitud no es un cambio de nombre de institución',
+});
 
 module.exports = createCambioNombreSolicitudPrograma;
